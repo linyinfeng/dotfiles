9c81c09ccf464d579c59ab79c35461d0a43652cc
Lin Yinfeng <lin.yinfeng@outlook.com>
2021-03-20 14:12:24 +0800

    Add cachix linyinfeng

---
+++ cachix/linyinfeng.nix
@@ -0,0 +1,10 @@
+{
+  nix = {
+    binaryCaches = [
+      "https://linyinfeng.cachix.org"
+    ];
+    binaryCachePublicKeys = [
+      "linyinfeng.cachix.org-1:sPYQXcNrnCf7Vr7T0YmjXz5dMZ7aOKG3EqLja0xr9MM="
+    ];
+  };
+}
