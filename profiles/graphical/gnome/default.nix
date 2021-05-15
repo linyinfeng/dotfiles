1e6e49debc2b93597c444c3c028074b4f59a7747
Lin Yinfeng <lin.yinfeng@outlook.com>
2021-05-15 13:04:02 +0800

    Update nixos version

--- profiles/graphical/gnome/default.nix
+++ profiles/graphical/gnome/default.nix
@@ -3,7 +3,7 @@
 {
   services.xserver.enable = true;
   services.xserver.displayManager.gdm.enable = true;
-  services.xserver.desktopManager.gnome3.enable = true;
+  services.xserver.desktopManager.gnome.enable = true;
 
   security.wrappers.spice-client-glib-usb-acl-helper.source =
     "${pkgs.spice-gtk}/bin/spice-client-glib-usb-acl-helper";
