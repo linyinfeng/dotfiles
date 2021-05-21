f5204682ca7c2995256f428128b97e4823d21bea
Lin Yinfeng <lin.yinfeng@outlook.com>
2021-05-21 10:45:43 +0800

    Add firewall rules for tailscale

--- profiles/networking/tailscale/default.nix
+++ profiles/networking/tailscale/default.nix
@@ -1,8 +1,11 @@
-{ ... }:
+{ config, ... }:
 
 {
   services.tailscale.enable = true;
   environment.global-persistence.directories = [
     "/var/lib/tailscale"
+  ];
+  networking.firewall.allowedUDPPorts = [
+    config.services.tailscale.port
   ];
 }
