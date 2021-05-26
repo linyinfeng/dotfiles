e74cacd096f10449b1be51e39c61047eca2a7e3d
Lin Yinfeng <lin.yinfeng@outlook.com>
2021-05-26 10:44:12 +0800

    Disable firewall settings for tailscale

--- profiles/networking/tailscale/default.nix
+++ profiles/networking/tailscale/default.nix
@@ -5,7 +5,8 @@
   environment.global-persistence.directories = [
     "/var/lib/tailscale"
   ];
+  # no need to open ports
   networking.firewall.allowedUDPPorts = [
-    config.services.tailscale.port
+    # config.services.tailscale.port
   ];
 }
