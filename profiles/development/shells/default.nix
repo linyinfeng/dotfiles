bb6d66ec04cb5402e454c62515dc84669f7f3a48
Lin Yinfeng <lin.yinfeng@outlook.com>
2021-06-23 17:38:29 +0800

    Add foreign-env

--- profiles/development/shells/default.nix
+++ profiles/development/shells/default.nix
@@ -3,6 +3,9 @@
 {
   programs.zsh.enable = true;
   programs.fish.enable = true;
+  environment.systemPackages = with pkgs; [
+    fishPlugins.foreign-env
+  ];
 
   environment.global-persistence.user.directories = [
     ".local/share/fish"
