2abbae355a83f170286580c7bdc1db1a65e1ccfd
Lin Yinfeng <lin.yinfeng@outlook.com>
2021-07-15 14:57:22 +0800

    Add emacs idris-mode

--- users/profiles/emacs/init.el
+++ users/profiles/emacs/init.el
@@ -151,6 +151,9 @@
   (setq lsp-rust-server 'rust-analyzer)
   (define-key lsp-mode-map (kbd "C-c l") lsp-command-map))
 
+(use-package idris-mode
+  :ensure t)
+
 (use-package lsp-ui
   :ensure t)
 
