891dedbd53ae8674fb07fbf253d4355eeb22b381
Lin Yinfeng <lin.yinfeng@outlook.com>
2021-08-13 14:31:07 +0800

    Update gitweb configuration

--- profiles/services/gitweb/default.nix
+++ profiles/services/gitweb/default.nix
@@ -1,7 +1,14 @@
 { config, ... }:
 
 {
-  services.nginx.gitweb.enable = true;
+  services.nginx.gitweb = {
+    enable = true;
+    group = config.users.groups.users.name;
+  };
+  services.gitweb.extraConfig = ''
+    $site_name = 'Git';
+    $project_maxdepth = 1;
+  '';
 
   environment.global-persistence.directories = [
     config.services.gitweb.projectroot
