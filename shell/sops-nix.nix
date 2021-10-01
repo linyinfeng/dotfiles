691994429c240e6639eeb35207b6b7098d1a4116
Lin Yinfeng <lin.yinfeng@outlook.com>
2021-10-01 11:01:52 +0800

    Update sops configuration

--- shell/sops-nix.nix
+++ shell/sops-nix.nix
@@ -6,17 +6,5 @@ in
   commands = map (pkgWithCategory "sops") (with pkgs; [
     sops
     ssh-to-age
-    ssh-to-pgp
-    # sops-import-keys-hook
   ]);
-
-  env = [
-    {
-      name = "sopsPGPKeyDirs";
-      value = toString [
-        ../secrets/keys/hosts
-        ../secrets/keys/users
-      ];
-    }
-  ];
 }
