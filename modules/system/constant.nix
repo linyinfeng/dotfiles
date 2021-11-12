28681471b09519f352e947c3e35e421c2d5a9069
Lin Yinfeng <lin.yinfeng@outlook.com>
2021-11-12 17:57:06 +0800

    Add an option to modules/system/constant
    Former-commit-id: 80226466c6b8a55b8ff600d993ddf31054b844a4

--- modules/system/constant.nix
+++ modules/system/constant.nix
@@ -1,17 +1,26 @@
 { lib, ... }:
 
 {
-  system.configurationRevision = lib.mkForce null;
-  nix.registry.self = lib.mkForce {
-    exact = false;
-    from = {
-      id = "self";
-      type = "indirect";
-    };
-    to = {
-      type = "github";
-      owner = "linyinfeng";
-      repo = "dotfiles";
+  options.system.constant = lib.mkOption {
+    type = lib.types.bool;
+    default = true;
+    description = ''
+      build a system with "constant" configuration revision
+    '';
+  };
+  config = {
+    system.configurationRevision = lib.mkForce null;
+    nix.registry.self = lib.mkForce {
+      exact = false;
+      from = {
+        id = "self";
+        type = "indirect";
+      };
+      to = {
+        type = "github";
+        owner = "linyinfeng";
+        repo = "dotfiles";
+      };
     };
   };
 }
