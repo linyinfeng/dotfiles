ff189a35fb844782d7e834056b6d2613316ed1e4
Lin Yinfeng <lin.yinfeng@outlook.com>
2021-11-13 23:07:26 +0800

    Use sops PR #138 and remove workaround
    Former-commit-id: 4099d233fcf4eafd609bd5bb577fda969ee2d7a7

--- modules/sops/defaults.nix
+++ modules/sops/defaults.nix
@@ -8,9 +8,4 @@
     then "/persist/etc/ssh/ssh_host_ed25519_key"
     else "/etc/ssh/ssh_host_ed25519_key")
   ];
-  # TODO workaround https://github.com/Mic92/sops-nix/issues/137
-  sops.extendScripts.pre-sops = ''
-    mkdir -p /run/secrets.d
-    mount -t ramfs -o nodev,nosuid,mode=651 none /run/secrets.d
-  '';
 }
