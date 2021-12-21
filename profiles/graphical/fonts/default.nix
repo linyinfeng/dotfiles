dfe96b115140a363899b6f674642c13c0742c8cf
Lin Yinfeng <lin.yinfeng@outlook.com>
2021-12-21 14:39:05 +0800

    Update font config
    Former-commit-id: b5b96216e724afb78b1ca53f707a7ae5e80f00f9

--- profiles/graphical/fonts/default.nix
+++ profiles/graphical/fonts/default.nix
@@ -7,7 +7,9 @@
     noto-fonts-emoji
     noto-fonts-extra
 
+    source-serif-pro
     source-han-serif
+    source-sans-pro
     source-han-sans
     source-code-pro
 
@@ -27,20 +29,18 @@
 
   fonts.fontconfig.defaultFonts = {
     sansSerif = lib.mkBefore [
+      "Source Sans Pro"
       "Source Han Sans SC"
       "Source Han Sans TC"
       "Source Han Sans HW"
       "Source Han Sans K"
-      "Noto Sans"
-      "DejaVu Sans"
     ];
     serif = lib.mkBefore [
+      "Source Serif Pro"
       "Source Han Serif SC"
       "Source Han Serif TC"
       "Source Han Serif HW"
       "Source Han Serif K"
-      "Noto Serif"
-      "DejaVu Serif"
     ];
     # respect powerline font settings in core
     monospace = lib.mkAfter [
