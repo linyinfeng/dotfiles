eefc6b201a284c998e9ad3c7564a99b7e6c8fa3d
Lin Yinfeng <lin.yinfeng@outlook.com>
2022-01-17 13:53:59 +0800

    Fix gnome-terminal font
    Former-commit-id: 97d90938bc951228b609081e6e814a8b38a7cfe1

--- users/profiles/gnome/default.nix
+++ users/profiles/gnome/default.nix
@@ -38,7 +38,7 @@ lib.mkIf
       profile.${defaultProfile} = {
         default = true;
         visibleName = "Main";
-        font = "Iosevka Yinfeng";
+        font = "Iosevka Yinfeng 10";
         scrollOnOutput = false;
       };
     };
