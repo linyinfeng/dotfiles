fd0d461a85b4f4cc51d112ebb3250d6d2899f076
Lin Yinfeng <lin.yinfeng@outlook.com>
2022-01-26 14:53:24 +0800

    Add suite phone for nuc
    Former-commit-id: 3bcb7fa7a4725324fdd6abcb4fd7e8bdbdd6e734

--- hosts/nuc/desktop/default.nix
+++ hosts/nuc/desktop/default.nix
@@ -4,6 +4,7 @@
   imports =
     suites.multimedia ++
     suites.game ++
+    suites.phone ++
     suites.printing;
   services.xserver.desktopManager.gnome.enable = true;
   services.xserver.displayManager.autoLogin = {
