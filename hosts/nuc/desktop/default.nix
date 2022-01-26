96bc375dad3ae5e6af3d3e8d04d9d7d7602223ae
Lin Yinfeng <lin.yinfeng@outlook.com>
2022-01-26 17:07:41 +0800

    Disable gdm auto suspend for nuc
    Former-commit-id: 187edc65ff5251ca8a61a8551dbadadfef2d7cfd

--- hosts/nuc/desktop/default.nix
+++ hosts/nuc/desktop/default.nix
@@ -7,6 +7,7 @@
     suites.phone ++
     suites.printing;
   services.xserver.desktopManager.gnome.enable = true;
+  services.xserver.displayManager.gdm.autoSuspend = false;
   services.xserver.displayManager.autoLogin = {
     enable = true;
     user = "yinfeng";
