41df2e832ecdd1bf8d6cba713c478362d322fb73
Lin Yinfeng <lin.yinfeng@outlook.com>
2022-01-26 17:20:44 +0800

    Do not auto login on nuc
    Former-commit-id: 860f89507554dcc44e46d2043bf10ff49d766edb

--- hosts/nuc/desktop/default.nix
+++ hosts/nuc/desktop/default.nix
@@ -8,8 +8,4 @@
     suites.printing;
   services.xserver.desktopManager.gnome.enable = true;
   services.xserver.displayManager.gdm.autoSuspend = false;
-  services.xserver.displayManager.autoLogin = {
-    enable = true;
-    user = "yinfeng";
-  };
 }
