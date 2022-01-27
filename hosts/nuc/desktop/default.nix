4099a2349ab6f222c1c6342ee784ac141b8c0a73
Lin Yinfeng <lin.yinfeng@outlook.com>
2022-01-27 15:34:22 +0800

    Add hidpi settings for nuc
    Former-commit-id: 919f060596656573ef6527f6a9ec211bc04304b0

--- hosts/nuc/desktop/default.nix
+++ hosts/nuc/desktop/default.nix
@@ -8,4 +8,9 @@
     suites.printing;
   services.xserver.desktopManager.gnome.enable = true;
   services.xserver.displayManager.gdm.autoSuspend = false;
+  hardware.video.hidpi.enable = true;
+  programs.steam.hidpi = {
+    enable = true;
+    scale = "2";
+  };
 }
