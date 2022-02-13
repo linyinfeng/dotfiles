8c3d86e78b6417428040dbb78d6037529d16c06d
Lin Yinfeng <lin.yinfeng@outlook.com>
2022-02-13 12:09:06 +0800

    Add easyeffects

--- profiles/services/pipewire/default.nix
+++ profiles/services/pipewire/default.nix
@@ -1,11 +1,23 @@
-{ ... }:
+{ pkgs, ... }:
 
 {
   services.pipewire = {
     enable = true;
+    wireplumber.enable = true;
+    media-session.enable = false;
+
+    # emulations
     pulse.enable = true;
     jack.enable = true;
     alsa.enable = true;
   };
   hardware.pulseaudio.enable = false;
+
+  environment.systemPackages = with pkgs; [
+    helvum
+    easyeffects
+  ];
+  environment.global-persistence.user.directories = [
+    ".config/easyeffects"
+  ];
 }
