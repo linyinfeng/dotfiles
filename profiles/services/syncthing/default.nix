5f7580519aec78c1bc66d654a4fea47347d9df78
Lin Yinfeng <lin.yinfeng@outlook.com>
2022-02-19 13:30:37 +0800

    Change syncthing path

--- profiles/services/syncthing/default.nix
+++ profiles/services/syncthing/default.nix
@@ -3,19 +3,19 @@
 let
   cfg = config.services.syncthing;
   devices = {
-    "t460p" = {
+    t460p = {
       id = "ESRNKCW-WFHWAZZ-H7YXVCM-YM43VOE-VIREZYF-EY7DKPO-UUZLHIH-RSHQBQR";
     };
-    "xps8930" = {
+    xps8930 = {
       id = "6TJQETQ-R4ST2CI-3O3K3K7-GSA3XLZ-B7WB7QU-H4UCP2H-ZOMV6KN-G7EF5QS";
     };
-    "nuc" = {
+    nuc = {
       id = "FN5AKLS-VLUOTUK-RTKQWQ2-M3DOLFK-OMB7VJD-KA627GA-M2TY435-QFFLOQE";
       addresses = [
         "tcp://nuc.li7g.com:22000"
       ];
     };
-    "k40" = {
+    k40 = {
       id = "BSTUP5D-LGMGCRC-MKM2OZO-SB5RYDK-5N73YYS-YTWEFPK-WWWD3IK-YFUU2QU";
     };
   };
@@ -35,14 +35,14 @@ in
     key = config.sops.secrets."syncthing/${hostName}".path;
     devices = others;
     folders = {
-      "/var/lib/syncthing/Sync" = {
-        id = "main";
+      "main" = {
+        path = "/var/lib/syncthing/Main";
         devices = otherNames;
         ignoreDelete = false;
         ignorePerms = false;
       };
-      "/home/yinfeng/Data/Music" = {
-        id = "music";
+      "music" = {
+        path = "/var/lib/syncthing/Music";
         devices = otherNames;
         ignoreDelete = false;
         ignorePerms = false;
