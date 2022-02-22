0eb695e55f3856cc865ab23ac941b5fa48631a78
Lin Yinfeng <lin.yinfeng@outlook.com>
2022-02-22 14:46:52 +0800

    Set priority for nuc/store

--- profiles/networking/fw-proxy/default.nix
+++ profiles/networking/fw-proxy/default.nix
@@ -29,7 +29,9 @@ in
     }
     (lib.mkIf (config.networking.hostName != "nuc") {
       settings.substituters = lib.mkOrder 1100 [
-        "https://nuc.li7g.com:8443/store"
+        # priority of cache.nixos.org and its mirror: 40
+        # priority of cachix: 41
+        "https://nuc.li7g.com:8443/store?priority=50"
       ];
       settings.trusted-public-keys = [
         "cache.li7g.com:YIVuYf8AjnOc5oncjClmtM19RaAZfOKLFFyZUpOrfqM="
