3d66aa80aa2402b47973ee83606b18bc48d4e904
Lin Yinfeng <lin.yinfeng@outlook.com>
2022-03-09 13:23:14 +0800

    Remove workaround for distrobox and thermald

--- overlays/distrobox.nix
+++
@@ -1,4 +0,0 @@
-# TODO https://nixpk.gs/pr-tracker.html?pr=160251
-channels: final: prev: {
-  inherit (channels.latest) distrobox;
-}
--- overlays/thermald.nix
+++
@@ -1,5 +0,0 @@
-# TODO https://nixpk.gs/pr-tracker.html?pr=162568
-channels: final: prev:
-{
-  inherit (channels.latest) thermald;
-}
