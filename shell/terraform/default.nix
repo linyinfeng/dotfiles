1d2eed393f3c62bfa562b240ec5ed29fc34fc03c
Lin Yinfeng <lin.yinfeng@outlook.com>
2022-04-04 19:46:37 +0800

    Remove terraform-env

--- shell/terraform/default.nix
+++ shell/terraform/default.nix
@@ -3,14 +3,10 @@
 let
   command = package: { category = "infrustructure"; inherit package; };
   terraform = pkgs.terraform;
-  terraform-env = pkgs.writeShellScriptBin "terraform-env" ''
-    ${pkgs.sops}/bin/sops exec-env ../secrets/terraform.yaml ${pkgs.fish}/bin/fish
-  '';
 in
 {
   commands = [
     (command terraform)
-    (command terraform-env)
     (command pkgs.nur.repos.linyinfeng.cf-terraforming)
   ];
 }
