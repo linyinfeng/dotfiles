1eb8d6f90b0eacb0f811dc4392552b6c2a42c7e3
Lin Yinfeng <lin.yinfeng@outlook.com>
2022-05-01 16:00:00 +0800

    Add flock for cache-li7g-com services

--- hosts/nuc/hydra/cache.nix
+++ hosts/nuc/hydra/cache.nix
@@ -9,25 +9,31 @@ in
       export AWS_ACCESS_KEY_ID=$(cat "$CREDENTIALS_DIRECTORY/cache-key-id")
       export AWS_SECRET_ACCESS_KEY=$(cat "$CREDENTIALS_DIRECTORY/cache-access-key")
 
-      roots=($(fd "^.*-all-checks$" "${hydraRootsDir}" --exec echo "/nix/store/{/}"))
+      (
+        echo "wait for lock"
+        flock 200
+        echo "enter critical section"
 
-      nix store sign "''${roots[@]}" --recursive --key-file "$CREDENTIALS_DIRECTORY/signing-key"
-      for root in "''${roots[@]}"; do
-        echo "push cache to cahche.li7g.com for hydra gcroot: $root"
-        proxychains4 -q \
-          nix copy --to "s3://cache?endpoint=minio-overlay.li7g.com" "$root" --verbose
-      done
+        roots=($(fd "^.*-all-checks$" "${hydraRootsDir}" --exec echo "/nix/store/{/}"))
+        nix store sign "''${roots[@]}" --recursive --key-file "$CREDENTIALS_DIRECTORY/signing-key"
+        for root in "''${roots[@]}"; do
+          echo "push cache to cahche.li7g.com for hydra gcroot: $root"
+          proxychains4 -q \
+            nix copy --to "s3://cache?endpoint=minio-overlay.li7g.com" "$root" --verbose
+        done
+      ) 200>/var/lib/cache-li7g-com/lock
     '';
     path = with pkgs; [
       config.nix.package
       fd
       proxychains
+      util-linux
     ];
     serviceConfig = {
       User = "hydra";
       Group = "hydra";
       Type = "oneshot";
-      StateDirectory = "copy-cache-li7g-com";
+      StateDirectory = "cache-li7g-com";
       Restart = "on-failure";
       LoadCredential = [
         "cache-key-id:${config.sops.secrets."cache/keyId".path}"
@@ -37,7 +43,7 @@ in
     };
     environment = lib.mkMerge [
       {
-        HOME = "/var/lib/copy-cache-li7g-com";
+        HOME = "/var/lib/cache-li7g-com";
       }
       (lib.mkIf (config.networking.fw-proxy.enable)
         config.networking.fw-proxy.environment)
@@ -45,19 +51,28 @@ in
   };
   systemd.services."gc-cache-li7g-com" = {
     script = ''
-      rm -rf /var/lib/copy-cache-li7g-com/.cache
       export AWS_ACCESS_KEY_ID=$(cat "$CREDENTIALS_DIRECTORY/cache-key-id")
       export AWS_SECRET_ACCESS_KEY=$(cat "$CREDENTIALS_DIRECTORY/cache-access-key")
-      nix-gc-s3 cache --endpoint https://minio.li7g.com --roots "${hydraRootsDir}"
+
+      (
+        echo "wait for lock"
+        flock 200
+        echo "enter critical section"
+
+        rm -rf /var/lib/cache-li7g-com/.cache
+        nix-gc-s3 cache --endpoint https://minio.li7g.com --roots "${hydraRootsDir}"
+      ) 200>/var/lib/cache-li7g-com/lock
     '';
     path = with pkgs; [
       nix-gc-s3
       config.nix.package
+      util-linux
     ];
     serviceConfig = {
       User = "hydra";
       Group = "hydra";
       Type = "oneshot";
+      StateDirectory = "cache-li7g-com";
       Restart = "on-failure";
       LoadCredential = [
         "cache-key-id:${config.sops.secrets."cache/keyId".path}"
