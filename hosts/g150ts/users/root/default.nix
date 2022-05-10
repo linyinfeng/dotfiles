297ff287aeb342c95e6cc3134bb146269df29fa5
Lin Yinfeng <lin.yinfeng@outlook.com>
2022-05-10 22:57:33 +0800

    Update ssh keys for g150ts

--- hosts/g150ts/users/root/default.nix
+++ hosts/g150ts/users/root/default.nix
@@ -2,12 +2,7 @@
 
 {
   users.users.root.openssh.authorizedKeys.keyFiles =
-    map (p: ./ssh/keys + p) [
-      /ip-mi10pro
-      /ip-y7000
-      /matrixlt1
-      /matrixlt2
-    ];
+    map (p: ./ssh/keys/${p}) (lib.attrNames (builtins.readDir ./ssh/keys));
 
   sops.secrets."user-password/root".sopsFile = lib.mkForce (config.sops.secretsDir + /g150t-s.yaml);
 }
