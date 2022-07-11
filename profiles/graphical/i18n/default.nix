659783f9b76947e6aa708e37653d3bbd56a3b32e
Lin Yinfeng <lin.yinfeng@outlook.com>
2022-07-11 21:33:18 +0800

    Add mozc

--- profiles/graphical/i18n/default.nix
+++ profiles/graphical/i18n/default.nix
@@ -4,18 +4,25 @@ lib.mkMerge [
   (lib.mkIf config.services.xserver.desktopManager.gnome.enable {
     i18n.inputMethod = {
       enabled = "ibus";
-      ibus.engines = [
-        pkgs.ibus-engines.rime
+      ibus.engines = with pkgs.ibus-engines; [
+        rime
+        mozc
       ];
     };
   })
   (lib.mkIf config.services.xserver.desktopManager.plasma5.enable {
     i18n.inputMethod = {
       enabled = "fcitx5";
-      fcitx5.addons = [
-        pkgs.fcitx5-rime
+      fcitx5.addons = with pkgs; [
+        fcitx5-rime
+        fcitx5-mozc
       ];
     };
   })
+  {
+    environment.global-persistence.user.directories = [
+      ".config/mozc"
+    ];
+  }
 ]
 
