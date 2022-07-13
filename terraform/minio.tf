1dfb6c3d996be23395a88558f582785ebf5a853a
Lin Yinfeng <lin.yinfeng@outlook.com>
2022-07-13 12:01:36 +0800

    Remove minio old backup buckets

--- terraform/minio.tf
+++ terraform/minio.tf
@@ -76,48 +76,6 @@ resource "minio_iam_user_policy_attachment" "cache" {
   user_name   = minio_iam_user.cache.name
 }
 
-# Backup bucket
-
-resource "minio_s3_bucket" "backup" {
-  bucket = "backup"
-  acl    = "private"
-}
-
-resource "minio_iam_user" "backup" {
-  name = "backup"
-}
-
-output "minio_backup_key_id" {
-  value     = minio_iam_user.backup.id
-  sensitive = false
-}
-output "minio_backup_access_key" {
-  value     = minio_iam_user.backup.secret
-  sensitive = true
-}
-
-data "minio_iam_policy_document" "backup" {
-  statement {
-    actions = [
-      "s3:*",
-    ]
-    resources = [
-      "arn:aws:s3:::backup/*",
-    ]
-  }
-}
-
-resource "minio_iam_policy" "backup" {
-  name   = "backup"
-  policy = data.minio_iam_policy_document.backup.json
-}
-
-resource "minio_iam_user_policy_attachment" "backup" {
-  policy_name = minio_iam_policy.backup.name
-  user_name   = minio_iam_user.backup.name
-}
-
-
 # Pastebin
 
 resource "minio_s3_bucket" "pastebin" {
