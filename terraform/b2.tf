84449a7ec593ededb6890bd818d230da72cc7847
Lin Yinfeng <lin.yinfeng@outlook.com>
2022-07-13 11:49:17 +0800

    Remove old backup buckets

--- terraform/b2.tf
+++ terraform/b2.tf
@@ -24,45 +24,6 @@ output "b2_s3_api_host" {
   value = module.b2_s3_api_url.host
 }
 
-resource "b2_bucket" "backup" {
-  bucket_name = "yinfeng-backup"
-  bucket_type = "allPrivate"
-
-  # keep only the last version of the file
-  lifecycle_rules {
-    file_name_prefix              = ""
-    days_from_uploading_to_hiding = null
-    days_from_hiding_to_deleting  = 1
-  }
-}
-
-resource "b2_application_key" "backup" {
-  key_name  = "backup"
-  bucket_id = b2_bucket.backup.id
-  capabilities = [
-    "deleteFiles",
-    "listAllBucketNames",
-    "listBuckets",
-    "listFiles",
-    "readBucketEncryption",
-    "readBuckets",
-    "readFiles",
-    "shareFiles",
-    "writeBucketEncryption",
-    "writeFiles"
-  ]
-}
-
-output "b2_backup_key_id" {
-  value     = b2_application_key.backup.application_key_id
-  sensitive = false
-}
-output "b2_backup_access_key" {
-  value     = b2_application_key.backup.application_key
-  sensitive = true
-}
-
-
 resource "b2_bucket" "cache" {
   bucket_name = "cache-li7g-com"
   bucket_type = "allPublic" # files avaliable to download
