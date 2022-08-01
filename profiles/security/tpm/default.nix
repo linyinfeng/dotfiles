48a94f2481e53114256f08a7ba9d6009967d1e5c
Lin Yinfeng <lin.yinfeng@outlook.com>
2022-08-01 14:06:05 +0800

    Disable broken tpm2.pkcs11

--- profiles/security/tpm/default.nix
+++ profiles/security/tpm/default.nix
@@ -4,7 +4,8 @@
   security.tpm2 = {
     enable = true;
     abrmd.enable = true;
-    pkcs11.enable = true;
+    # TODO broken https://github.com/NixOS/nixpkgs/pull/183798
+    # pkcs11.enable = true;
   };
 
   environment.systemPackages = with pkgs; [
