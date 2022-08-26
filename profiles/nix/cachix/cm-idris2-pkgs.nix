4b8449b66036e7ed45ce251179d7354849305c4b
Lin Yinfeng <lin.yinfeng@outlook.com>
2022-08-26 15:09:33 +0800

    Add cachix cm-idris2-pkgs.nix

---
+++ profiles/nix/cachix/cm-idris2-pkgs.nix
@@ -0,0 +1,10 @@
+{
+  nix.settings = {
+    substituters = [
+      "https://cm-idris2-pkgs.cachix.org"
+    ];
+    trusted-public-keys = [
+      "cm-idris2-pkgs.cachix.org-1:YB2oJSEsD5oMJjAESxolC2GQtE6B5I6jkWhte2gtXjk="
+    ];
+  };
+}
--- profiles/nix/cachix/linyinfeng.nix
+++ profiles/nix/cachix/linyinfeng.nix
@@ -1,9 +1,9 @@
 {
-  nix = {
-    settings.substituters = [
+  nix.settings = {
+    substituters = [
       "https://linyinfeng.cachix.org"
     ];
-    settings.trusted-public-keys = [
+    trusted-public-keys = [
       "linyinfeng.cachix.org-1:sPYQXcNrnCf7Vr7T0YmjXz5dMZ7aOKG3EqLja0xr9MM="
     ];
   };
