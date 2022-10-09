9dc44288dbfb57bd7d055f0bcf74c2dd22040d70
Lin Yinfeng <lin.yinfeng@outlook.com>
2022-10-09 21:40:30 +0800

    Disable hardware.video.hidpi for framework

--- hosts/framework.nix
+++ hosts/framework.nix
@@ -50,7 +50,7 @@ in
   boot.kernelModules = [ "kvm-intel" ];
 
   hardware.enableRedistributableFirmware = true;
-  hardware.video.hidpi.enable = true;
+  # hardware.video.hidpi.enable = true;
   programs.steam.hidpi = {
     enable = true;
     scale = "2";
