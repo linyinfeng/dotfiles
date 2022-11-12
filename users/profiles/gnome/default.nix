391ac60642e7b44f66980151adb925f74b376989
Lin Yinfeng <lin.yinfeng@outlook.com>
2022-11-12 17:11:44 +0800

    More gnome settings

--- users/profiles/gnome/default.nix
+++ users/profiles/gnome/default.nix
@@ -4,6 +4,7 @@ let
     gsconnect
     appindicator
     workspace-indicator-2
+    dash-to-dock
   ];
 in
 lib.mkIf osConfig.services.xserver.desktopManager.gnome.enable
@@ -27,12 +28,29 @@ lib.mkIf osConfig.services.xserver.desktopManager.gnome.enable
       "org/gnome/shell" = {
         disable-user-extensions = false;
         enabled-extensions = map (p: p.extensionUuid) extensionPkgs;
+        disabled-extensions = [ ];
         favorite-apps = lib.mkBefore [
           "org.gnome.Console.desktop"
           "org.gnome.Nautilus.desktop"
           "chromium-browser.desktop"
           "firefox.desktop"
+          "gnome-system-monitor.desktop"
+          "code.desktop"
         ];
+      };
+      "org/gnome/desktop/interface" = {
+        clock-show-weekday = true;
+        show-battery-percentage = true;
+        locate-pointer = true;
+      };
+      # just use the standard touchpad and mouse speed
+      "org/gnome/desktop/peripherals/mouse" = {
+        speed = 0;
+      };
+      "org/gnome/desktop/peripherals/touchpad" = {
+        speed = 0;
+        natural-scroll = true;
+        tap-to-click = true;
       };
       "org/gnome/desktop/remote-desktop/rdp" =
         lib.mkIf (osConfig.security.acme.certs ? "main") {
@@ -44,6 +62,18 @@ lib.mkIf osConfig.services.xserver.desktopManager.gnome.enable
       "org/gnome/gnome-session" = {
         auto-save-session = true;
       };
+      "org/gnome/desktop/calendar" = {
+        show-weekdate = true;
+      };
+      "org/gnome/shell/extensions/dash-to-dock" =
+        {
+          apply-custom-theme = true;
+          custom-theme-shrink = true;
+          dash-max-icon-size = 32;
+          show-mounts = false;
+          scroll-action = "switch-workspace";
+          intellihide-mode = "ALL_WINDOWS";
+        };
     }
     (
       let
