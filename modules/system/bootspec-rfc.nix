87173dca93a6eae4d8841b7a4d8eb89dffd16e1f
Lin Yinfeng <lin.yinfeng@outlook.com>
2022-12-02 13:20:49 +0800

    Update bootspec-rfc

--- modules/system/bootspec-rfc.nix
+++ modules/system/bootspec-rfc.nix
@@ -1,23 +1,22 @@
 # TODO wait for https://nixpk.gs/pr-tracker.html?pr=185116
 { inputs, config, lib, pkgs, ... }:
 
-# bootspec-rfc
-let
-  children = lib.mapAttrs
-    (childName: childConfig: childConfig.configuration.system.build.toplevel)
-    config.specialisation;
-  bootSpec = import "${inputs.bootspec-rfc}/nixos/modules/system/activation/bootspec.nix" {
-    inherit config pkgs lib children;
-  };
-in
 {
   # bootspec-rfc
   imports = [
     "${inputs.bootspec-rfc}/nixos/modules/system/boot/loader/external/external.nix"
+    "${inputs.bootspec-rfc}/nixos/modules/system/activation/bootspec.nix"
   ];
   system.extraSystemBuilderCmds = ''
     ${lib.optionalString (!config.boot.isContainer) ''
-      ${bootSpec.writer}
+      ${config.boot.bootspec.writer}
+      ${config.boot.bootspec.validator} "$out/bootspec/${config.boot.bootspec.filename}"
     ''}
   '';
+  nixpkgs.overlays = [
+    (final: prev:
+      let system = final.stdenv.hostPlatform.system; in {
+        inherit (inputs.bootspec-rfc.legacyPackages.${system}) writeCueValidator;
+      })
+  ];
 }
