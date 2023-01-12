3b437d6512c1bb2bf8895d5471ef20b56b715348
Lin Yinfeng <lin.yinfeng@outlook.com>
2023-01-12 13:10:49 +0800

    Enable kernel module sign and lockdown

--- profiles/boot/secure-boot/default.nix
+++ profiles/boot/secure-boot/default.nix
@@ -7,24 +7,24 @@
     privateKeyFile = "/sbkeys/generated/db.key";
   };
 
-  # boot.kernelParams = [
-  #   "lockdown=integrity"
-  # ];
-  # boot.kernelPatches = [
-  #   # this patch makes the linux kernel unreproducible
-  #   {
-  #     name = "lockdown";
-  #     patch = null;
-  #     extraConfig = ''
-  #       MODULE_SIG y
-  #       SECURITY_LOCKDOWN_LSM y
-  #     '';
-  #   }
-  # ];
-  # assertions = [
-  #   {
-  #     assertion = lib.length config.boot.extraModulePackages == 0;
-  #     message = "out-of-tree and unsigned kernel module not supported";
-  #   }
-  # ];
+  boot.kernelParams = [
+    "lockdown=integrity"
+  ];
+  boot.kernelPatches = [
+    # this patch makes the linux kernel unreproducible
+    {
+      name = "lockdown";
+      patch = null;
+      extraConfig = ''
+        MODULE_SIG y
+        SECURITY_LOCKDOWN_LSM y
+      '';
+    }
+  ];
+  assertions = [
+    {
+      assertion = lib.length config.boot.extraModulePackages == 0;
+      message = "out-of-tree and unsigned kernel module not supported";
+    }
+  ];
 }
