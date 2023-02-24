e11af4eb5183cae303594d79f9f09096c548de13
Lin Yinfeng <lin.yinfeng@outlook.com>
2023-02-24 15:39:33 +0800

    Update commit-notifier

--- hosts/rica/commit-notifier/default.nix
+++ hosts/rica/commit-notifier/default.nix
@@ -4,10 +4,17 @@
   services.commit-notifier = {
     enable = true;
     cron = "0 */5 * * * *";
-    tokenFile = config.sops.secrets."telegram-bot/commit-notifier".path;
+    tokenFiles = {
+      telegramBot = config.sops.secrets."telegram-bot/commit-notifier".path;
+      github = config.sops.secrets."nano/github-token".path;
+    };
   };
   sops.secrets."telegram-bot/commit-notifier" = {
     sopsFile = config.sops-file.host;
+    restartUnits = [ "commit-notifier.service" ];
+  };
+  sops.secrets."nano/github-token" = {
+    sopsFile = config.sops-file.get "common.yaml";
     restartUnits = [ "commit-notifier.service" ];
   };
 
