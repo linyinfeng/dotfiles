539b0bd5020cd8e69fa174a37a4541225d67049f
Lin Yinfeng <lin.yinfeng@outlook.com>
2023-03-09 12:05:40 +0800

    Set proxy variables for fish login shell

--- users/profiles/shells/default.nix
+++ users/profiles/shells/default.nix
@@ -1,9 +1,14 @@
-{ config, pkgs, lib, ... }:
+{ config, pkgs, lib, osConfig, ... }:
 
 {
   programs.fish.enable = true;
   programs.skim.enable = true;
   programs.zoxide.enable = true;
+
+  programs.fish.interactiveShellInit =
+    lib.mkIf osConfig.networking.fw-proxy.enable ''
+      fenv source enable-proxy
+    '';
 
   home.global-persistence.directories = [
     ".local/share/zoxide"
