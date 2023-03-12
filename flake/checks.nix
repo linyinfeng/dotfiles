ca5e58845ae4ed2e8ac7ece6092decd99e55d23e
Lin Yinfeng <lin.yinfeng@outlook.com>
2023-03-12 21:42:41 +0800

    Add all-checks

--- flake/checks.nix
+++ flake/checks.nix
@@ -1,6 +1,7 @@
 {
   self,
   lib,
+  withSystem,
   ...
 }: let
   getHostToplevel = name: cfg: let
@@ -11,10 +12,22 @@
   hostToplevels =
     lib.fold lib.recursiveUpdate {}
     (lib.mapAttrsToList getHostToplevel self.nixosConfigurations);
+  checks = hostToplevels;
+  hydraMachine = "nuc";
+  hydraSystem = self.nixosConfigurations.${hydraMachine}.pkgs.stdenv.hostPlatform.system;
+  allChecks = withSystem hydraSystem ({pkgs, ...}:
+    pkgs.linkFarm "all-checks"
+    (lib.flatten (lib.mapAttrsToList
+      (system:
+        lib.mapAttrsToList (name: drv: {
+          name = "${name}-${system}";
+          path = drv;
+        }))
+      checks)));
 in {
-  passthru = {
-    # inherit hostToplevels;
-    test = lib.mapAttrsToList getHostToplevel self.nixosConfigurations;
-  };
-  flake.checks = hostToplevels;
+  flake.checks =
+    lib.recursiveUpdate checks
+    {
+      "${hydraSystem}".all-checks = allChecks;
+    };
 }
