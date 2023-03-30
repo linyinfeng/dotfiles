3afc1231f62635aebdc376d774549b4a27ad60b6
Lin Yinfeng <lin.yinfeng@outlook.com>
2023-03-30 16:12:54 +0800

    cowrie: migrate to systemd service

--- nixos/profiles/services/ssh-honeypot/default.nix
+++ nixos/profiles/services/ssh-honeypot/default.nix
@@ -1,34 +1,87 @@
 {
   config,
+  pkgs,
   lib,
   ...
 }: let
   port = config.ports.ssh-honeypot;
+  cowrieSrc = pkgs.stdenv.mkDerivation {
+    inherit (pkgs.nur.repos.linyinfeng.sources.cowrie) pname version src;
+    patches = [
+      ./cowrie-telegram-output-requests.patch
+    ];
+    installPhase = ''
+      cp -r . $out
+    '';
+  };
 in {
-  virtualisation.oci-containers.containers."ssh-honeypot" = {
-    image = "docker.io/cowrie/cowrie:latest";
-    extraOptions = ["--label" "io.containers.autoupdate=registry"];
-    ports = ["${toString port}:2222/tcp"];
+  passthru = { inherit cowrieSrc; };
+  systemd.services.cowrie = {
+    # setting up environment as
+    # https://github.com/cowrie/cowrie/blob/master/docker/Dockerfile
+    script = ''
+      rsync --verbose --recursive --delete "${cowrieSrc}/" cowrie-src/
+      chmod -R u+w cowrie-src
+
+      mkdir -p state share log honeyfs txtcmds
+      rsync --verbose --recursive cowrie-src/var/lib/cowrie/ state/
+      rsync --verbose --recursive cowrie-src/honeyfs/ honeyfs/
+      if [ -f share/cowrie/fs.pickle ]; then
+        echo "backup fs.pickle"
+        mv share/cowrie/fs.pickle share/cowrie/fs.pickle.bak
+      fi
+      rsync --verbose --recursive --delete cowrie-src/share/ share/
+      if [ -f share/cowrie/fs.pickle.bak ]; then
+        echo "restore fs.pickle"
+        mv share/cowrie/fs.pickle.bak share/cowrie/fs.pickle
+      fi
+
+      python3 -m venv cowrie-venv
+      source cowrie-venv/bin/activate
+      pip install --no-cache-dir --upgrade pip wheel setuptools
+      pip install --no-cache-dir --upgrade cffi
+      pip install --no-cache-dir --upgrade --requirement cowrie-src/requirements.txt
+      pip install --no-cache-dir --upgrade --requirement cowrie-src/requirements-output.txt
+
+      export PYTHONPATH=cowrie-src/src
+      python3 -m compileall cowrie-src
+      python3 cowrie-venv/bin/twistd --umask=0022 --nodaemon --pidfile= --logfile=- cowrie
+    '';
+    path = with pkgs; [
+      python3
+      gcc
+      rsync
+    ];
+    serviceConfig = {
+      DynamicUser = true;
+      StateDirectory = "cowrie";
+      WorkingDirectory = "/var/lib/cowrie";
+      EnvironmentFile = [
+        config.sops.templates."cowrie-extra-env".path
+      ];
+      AmbientCapabilities = [
+        "CAP_NET_BIND_SERVICE"
+      ];
+    };
     environment =
       {
+        COWRIE_SSH_LISTEN_ENDPOINTS = "tcp6:22:interface=\\:\\:";
+        COWRIE_HONEYPOT_STATE_PATH = "state";
+        COWRIE_HONEYPOT_SHARE_PATH = "share/cowrie";
+        COWRIE_HONEYPOT_LOG_PATH = "log";
+        COWRIE_HONEYPOT_CONTENTS_PATH = "honeyfs";
+        COWRIE_HONEYPOT_TXTCMDS_PATH = "txtcmds";
         COWRIE_HONEYPOT_HOSTNAME = config.networking.hostName;
         COWRIE_HONEYPOT_TIMEZONE = config.time.timeZone;
         COWRIE_HONEYPOT_AUTH_CLASS = "AuthRandom";
         COWRIE_SHELL_KERNEL_VERSION = "5.10.0-20-amd64";
         COWRIE_SHELL_KERNEL_BUILD_STRING = "#1 SMP Debian 5.10.158-2 (2022-12-13)";
-      };
-    environmentFiles = [
-      config.sops.templates."cowrie-extra-env".path
-    ];
-    volumes = [
-      "/var/lib/cowrie:/cowrie/var/lib/cowrie"
-      "/var/log/cowrie:/cowrie/var/log/cowrie"
-    ];
+      }
+      // lib.optionalAttrs (config.networking.fw-proxy.enable)
+      config.networking.fw-proxy.environment;
+    after = ["network-online.target"];
+    wantedBy = [ "multi-user.target" ];
   };
-  systemd.tmpfiles.rules = [
-    "d /var/lib/cowrie 700 root root - -"
-    "d /var/log/cowrie 700 root root - -"
-  ];
   sops.templates."cowrie-extra-env".content = ''
     COWRIE_OUTPUT_TELEGRAM_ENABLED=true
     COWRIE_OUTPUT_TELEGRAM_BOT_TOKEN=${config.sops.placeholder."telegram-bot/push"}
