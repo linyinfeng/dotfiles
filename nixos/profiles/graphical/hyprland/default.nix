c645703d757c773c9e2390c3b8d14e1934ceff08
Lin Yinfeng <lin.yinfeng@outlook.com>
2023-04-16 17:31:41 +0800

    Coexists of GNOME, Hyprland and Sway

--- nixos/profiles/graphical/hyprland/default.nix
+++ nixos/profiles/graphical/hyprland/default.nix
@@ -1,40 +1,60 @@
-{pkgs, ...}: {
-  programs.hyprland.enable = true;
-  environment.sessionVariables = {
-    NIXOS_OZONE_WL = ""; # input method not working
-  };
+{
+  pkgs,
+  config,
+  lib,
+  ...
+}:
+lib.mkMerge [
+  {
+    programs.hyprland.enable = true;
+    environment.sessionVariables = {
+      NIXOS_OZONE_WL = ""; # input method not working
+    };
 
-  security.pam.services.swaylock.text = ''
-    account required pam_unix.so
+    systemd.user.services.xdg-desktop-portal-hyprland = {
+      unitConfig = {
+        ConditionEnvironment = "HYPRLAND_INSTANCE_SIGNATURE";
+      };
+    };
+    systemd.user.services.xdg-desktop-portal-gnome = {
+      unitConfig = {
+        ConditionEnvironment = "XDG_CURRENT_DESKTOP=GNOME";
+      };
+    };
 
-    # check passwork before fprintd
-    auth sufficient pam_unix.so try_first_pass likeauth
-    auth sufficient ${pkgs.fprintd}/lib/security/pam_fprintd.so
-    auth required pam_deny.so
+    security.pam.services.swaylock.text = ''
+      account required pam_unix.so
 
-    password sufficient pam_unix.so nullok yescrypt
+      # check passwork before fprintd
+      auth sufficient pam_unix.so try_first_pass likeauth
+      auth sufficient ${pkgs.fprintd}/lib/security/pam_fprintd.so
+      auth required pam_deny.so
 
-    session required pam_env.so conffile=/etc/pam/environment readenv=0
-    session required pam_unix.so
-  '';
+      password sufficient pam_unix.so nullok yescrypt
 
-  environment.systemPackages = with pkgs; [
-    light
-  ];
-  security.sudo.extraRules = [
-    {
-      groups = ["users"];
-      commands = [
-        {
-          command = "/run/current-system/sw/bin/light";
-          options = ["NOPASSWD"];
-        }
-      ];
-    }
-  ];
+      session required pam_env.so conffile=/etc/pam/environment readenv=0
+      session required pam_unix.so
+    '';
 
-  services.gnome.gnome-keyring.enable = true;
-  environment.global-persistence.user.directories = [
-    ".local/share/keyrings"
-  ];
-}
+    environment.systemPackages = with pkgs; [
+      light
+    ];
+    security.sudo.extraRules = [
+      {
+        groups = ["users"];
+        commands = [
+          {
+            command = "/run/current-system/sw/bin/light";
+            options = ["NOPASSWD"];
+          }
+        ];
+      }
+    ];
+  }
+  (lib.mkIf (!config.services.xserver.desktopManager.gnome.enable) {
+    services.gnome.gnome-keyring.enable = true;
+    environment.global-persistence.user.directories = [
+      ".local/share/keyrings"
+    ];
+  })
+]
--- nixos/profiles/graphical/sway/default.nix
+++ nixos/profiles/graphical/sway/default.nix
@@ -2,7 +2,7 @@
   programs.sway = {
     enable = true;
     extraPackages = with pkgs; [
-      swaylock
+      swaylock-effects
       swayidle
     ];
   };
