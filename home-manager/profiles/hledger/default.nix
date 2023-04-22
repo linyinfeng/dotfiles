f13335ccaad11a208fdb9a00f00cf23d5f1a881a
Lin Yinfeng <lin.yinfeng@outlook.com>
2023-04-22 12:44:32 +0800

    Add home-manager profiles hledger

---
+++ home-manager/profiles/hledger/default.nix
@@ -0,0 +1,10 @@
+{ pkgs, ... }:
+{
+  home.packages = with pkgs; [
+    hledger
+    hledger-ui
+  ];
+  home.sessionVariables = {
+    "LEDGER_FILE" = "$HOME/Syncthing/Main/ledger/main.journal";
+  };
+}
