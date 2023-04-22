5008a32d1f0f3ee5c4da68beb61b981483eb7594
Lin Yinfeng <lin.yinfeng@outlook.com>
2023-04-22 14:58:46 +0800

    Refactor fixes overlay to earlyFixes and lateFixes

--- flake/nixpkgs.nix
+++ flake/nixpkgs.nix
@@ -42,8 +42,15 @@
         hydra-master = inputs'.hydra.packages.default;
       })
   ];
+  earlyFixes = final: prev: let
+    inherit (prev.stdenv.hostPlatform) system;
+    inherit ((getSystem system).allModuleArgs) inputs';
+  in {
+    # TODO wait for https://nixpk.gs/pr-tracker.html?pr=226283
+    inherit (inputs'.nixpkgs-wayland.legacyPackages) wayland;
+  };
 
-  fixes = final: prev: let
+  lateFixes = final: prev: let
     inherit (prev.stdenv.hostPlatform) system;
     inherit ((getSystem system).allModuleArgs) inputs';
   in {
@@ -65,7 +72,6 @@
     inherit (inputs'.nixpkgs-wluma.legacyPackages) wluma;
 
     # TODO wait for https://nixpk.gs/pr-tracker.html?pr=226283
-    inherit (inputs'.nixpkgs-wayland.legacyPackages) wayland;
     hyprland = prev.hyprland.override {
       hidpiXWayland = true;
     };
@@ -75,10 +81,6 @@ in {
     config = {
       allowUnfree = true;
     };
-    overlays =
-      packages
-      ++ [
-        fixes
-      ];
+    overlays = [earlyFixes] ++ packages ++ [lateFixes];
   };
 }
