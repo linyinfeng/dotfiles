ea6bbc741597fba43ba9a29268bdfe9eefc1ea36
Lin Yinfeng <lin.yinfeng@outlook.com>
2023-05-09 17:20:42 +0800

    networking/dn42: disable rp_filter

--- nixos/modules/networking/dn42/default.nix
+++ nixos/modules/networking/dn42/default.nix
@@ -304,6 +304,10 @@ in {
       "net.ipv4.conf.default.forwarding" = 1;
       "net.ipv6.conf.all.forwarding" = 1;
       "net.ipv4.conf.all.forwarding" = 1;
+      # disable rp_filter
+      "net.ipv4.conf.all.rp_filter" = 0;
+      "net.ipv4.conf.default.rp_filter" = 0;
+      "net.ipv4.conf.*.rp_filter" = 0;
     };
     networking.firewall.checkReversePath = false;
 
