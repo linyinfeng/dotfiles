ea3b30a2c63ff4300abf52906825d040b6960159
Lin Yinfeng <lin.yinfeng@outlook.com>
2023-05-10 12:07:13 +0800

    networking/dn42: next hop self ebgp only

--- nixos/modules/networking/dn42/_bgp.nix
+++ nixos/modules/networking/dn42/_bgp.nix
@@ -112,7 +112,7 @@ in
               };
               export filter { if is_valid_network_v4() && source ~ [RTS_STATIC, RTS_BGP] then accept; else reject; };
               import table;
-              next hop self;
+              next hop self ebgp;
               igp table mesh_v4;
             };
             ipv6 {
