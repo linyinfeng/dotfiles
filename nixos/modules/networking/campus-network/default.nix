bfeb5f225b9590ae059f1fe0848efe3528252a9e
Lin Yinfeng <lin.yinfeng@outlook.com>
2023-05-25 13:43:18 +0800

    networking/behind-fw: use mirror-z as substituter

--- nixos/modules/networking/campus-network/default.nix
+++ nixos/modules/networking/campus-network/default.nix
@@ -70,7 +70,6 @@ in {
       sopsFile = config.sops-file.get "common.yaml";
       restartUnits = ["campus-net-auto-login.service"];
     };
-    # nix.settings.substituters = lib.mkOrder 500 ["https://mirrors.nju.edu.cn/nix-channels/store"];
 
     systemd.services."campus-net-auto-login" = {
       enable = cfg.auto-login.enable;
--- nixos/profiles/networking/behind-fw/default.nix
+++ nixos/profiles/networking/behind-fw/default.nix
@@ -4,7 +4,6 @@
   ...
 }: {
   nix.settings.substituters = lib.mkOrder 900 [
-    # TODO tuna is slow currently
-    # "https://mirrors.tuna.tsinghua.edu.cn/nix-channels/store"
+    "https://mirrors.cernet.edu.cn/nix-channels/store"
   ];
 }
