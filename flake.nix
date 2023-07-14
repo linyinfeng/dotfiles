56253dc287acbbadf4fc85fc4a14e6b7ac5cddb0
Lin Yinfeng <lin.yinfeng@outlook.com>
2023-07-14 17:28:52 +0800

    inputs/mobile-nixos: use linyinfeng/-/enchilada-staging

--- flake.nix
+++ flake.nix
@@ -211,7 +211,7 @@
 
     # TODO wait for https://github.com/NixOS/mobile-nixos/pull/627
     # mobile-nixos.url = "github:nixos/mobile-nixos";
-    mobile-nixos.url = "github:linyinfeng/mobile-nixos/enchilada";
+    mobile-nixos.url = "github:linyinfeng/mobile-nixos/enchilada-staging";
     mobile-nixos.flake = false;
 
     # fixes
