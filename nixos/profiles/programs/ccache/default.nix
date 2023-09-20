278043777c09f645aa16d8646a59809fe79d5167
Lin Yinfeng <lin.yinfeng@outlook.com>
2023-09-21 00:36:34 +0800

    programs/ccache: fix typo

--- nixos/profiles/programs/ccache/default.nix
+++ nixos/profiles/programs/ccache/default.nix
@@ -5,6 +5,6 @@ in {
   nix.settings.extra-sandbox-paths = [cfg.cacheDir];
   environment.global-persistence.directories = [cfg.cacheDir];
   systemd.tmpfiles.rules = [
-    "z ${cfg.cacheDir} 770 root nixbuild - -"
+    "z ${cfg.cacheDir} 770 root nixbld - -"
   ];
 }
