81450b3aa31566dd4221089ec3b76fb498c0f76c
Lin Yinfeng <lin.yinfeng@outlook.com>
2023-09-21 00:20:32 +0800

    programs/ccache: fix permission of cache directory

--- nixos/profiles/programs/ccache/default.nix
+++ nixos/profiles/programs/ccache/default.nix
@@ -4,4 +4,7 @@ in {
   programs.ccache.enable = true;
   nix.settings.extra-sandbox-paths = [cfg.cacheDir];
   environment.global-persistence.directories = [cfg.cacheDir];
+  systemd.tmpfiles.rules = [
+    "z ${cfg.cacheDir} 770 root nixbuild - -"
+  ];
 }
