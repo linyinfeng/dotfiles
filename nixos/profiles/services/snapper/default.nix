ee35ceeeafa3e6df7132cd22e042957dddc9c386
Lin Yinfeng <lin.yinfeng@outlook.com>
2023-09-22 17:16:27 +0800

    services/snapper: auto create .snapshot subvolumes

--- nixos/profiles/services/snapper/default.nix
+++ nixos/profiles/services/snapper/default.nix
@@ -1,5 +1,14 @@
-{ ... }:
-
+{ config, pkgs, lib, ... }:
+let
+  subvolumes = lib.lists.map (v: v.SUBVOLUME) (lib.attrValues config.services.snapper.configs);
+  createForSubvolume = subvol: let
+    target = "${subvol}/.snapshots";
+  in ''
+    if [ ! -e "${target}" ]; then
+      btrfs subvolume create "${target}"
+    fi
+  '';
+in
 {
   services.snapper.configs = {
     persist = {
@@ -16,5 +25,16 @@
       TIMELINE_LIMIT_MONTHLY = 2;
       TIMELINE_LIMIT_YEARLY = 0;
     };
+  };
+  systemd.services.snapper-subvolumes = {
+    script = lib.concatMapStringsSep "\n" createForSubvolume subvolumes;
+    serviceConfig = {
+      Type = "oneshot";
+    };
+    path = with pkgs; [
+      btrfs-progs
+    ];
+    requiredBy = ["snapper-timeline.service"];
+    before = ["snapper-timeline.service"];
   };
 }
