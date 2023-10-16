7d07f27232cc8a9b6f990ae2f010f07976037976
Lin Yinfeng <lin.yinfeng@outlook.com>
2023-10-16 17:04:02 +0800

    dev/shells: persist .bash_history

--- nixos/profiles/development/shells/default.nix
+++ nixos/profiles/development/shells/default.nix
@@ -1,24 +1,37 @@
-{pkgs, ...}: {
-  programs.fish.enable = true;
-  environment.systemPackages =
-    (with pkgs.fishPlugins; [
-      foreign-env
-      done
-      autopair-fish
-    ])
-    ++ (with pkgs.nur.repos.linyinfeng.fishPlugins; [
-      git
-      bang-bang
-      replay
-    ])
-    ++ (with pkgs; [
-      libnotify # for done notification
-      comma
-    ]);
+{
+  pkgs,
+  lib,
+  ...
+}:
+lib.mkMerge [
+  # fish
+  {
+    programs.fish.enable = true;
+    environment.systemPackages =
+      (with pkgs.fishPlugins; [
+        foreign-env
+        done
+        autopair-fish
+      ])
+      ++ (with pkgs.nur.repos.linyinfeng.fishPlugins; [
+        git
+        bang-bang
+        replay
+      ])
+      ++ (with pkgs; [
+        libnotify # for done notification
+        comma
+      ]);
 
-  programs.tprofile.enable = true;
+    environment.global-persistence.user.directories = [
+      ".local/share/fish"
+    ];
+  }
 
-  environment.global-persistence.user.directories = [
-    ".local/share/fish"
-  ];
-}
+  # bash
+  {
+    environment.global-persistence.user.files = [
+      ".bash_history"
+    ];
+  }
+]
