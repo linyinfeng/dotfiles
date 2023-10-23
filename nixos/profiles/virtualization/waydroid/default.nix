aa1eecc63d818c3f9ceda4841df73e81ce6dce1e
Lin Yinfeng <lin.yinfeng@outlook.com>
2023-10-23 16:22:38 +0800

    waydroid: emulate aarch64-linux and armv7l-linux

--- nixos/profiles/virtualization/waydroid/default.nix
+++ nixos/profiles/virtualization/waydroid/default.nix
@@ -9,6 +9,10 @@
     [General]
     ApiLevel = 30
   '';
+  boot.binfmt.emulatedSystems = [
+    "aarch64-linux"
+    "armv7l-linux"
+  ];
   environment.global-persistence.user.directories = [
     ".local/share/waydroid"
   ];
