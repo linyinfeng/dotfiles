c3c3558d85322534b097d7eef8605198a58e4ba6
Lin Yinfeng <lin.yinfeng@outlook.com>
2023-11-01 19:26:35 +0800

    networking/resolved: force dns over tls and set dns servers manually

--- nixos/profiles/networking/resolved/default.nix
+++ nixos/profiles/networking/resolved/default.nix
@@ -3,23 +3,36 @@
   lib,
   ...
 }:
+let
+  dnsServers = [
+    # Cloudflare public DNS
+    "1.1.1.1"
+    "1.0.0.1"
+    "2606:4700:4700::1111"
+    "2606:4700:4700::1001"
+    # Google public DNS
+    "8.8.8.8"
+    "8.8.4.4"
+    "2001:4860:4860::8888"
+    "2001:4860:4860::8844"
+    # TUNA DNS666
+    "2001:da8::666"
+    # TWNIC
+    "101.101.101.101"
+    "101.102.103.104"
+  ];
+in
 lib.mkMerge [
   {
     services.resolved = {
       enable = true;
       dnssec = "allow-downgrade";
-      fallbackDns = [
-        # Google public DNS
-        "8.8.8.8"
-        "8.8.4.4"
-        "2001:4860:4860::8888"
-        "2001:4860:4860::8844"
-        # Cloudflare public DNS
-        "1.1.1.1"
-        "1.0.0.1"
-        "2606:4700:4700::1111"
-        "2606:4700:4700::1001"
-      ];
+      llmnr = "true";
+      fallbackDns = [];
+      extraConfig = ''
+        DNS=${lib.concatStringsSep " " dnsServers}
+        DNSOverTLS=yes
+      '';
     };
     networking.firewall.allowedUDPPorts = [
       5353
