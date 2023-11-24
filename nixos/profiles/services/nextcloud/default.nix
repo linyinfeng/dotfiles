7b67a28a2d92056ab8e0dec77acbd0ac0e4deda1
Lin Yinfeng <lin.yinfeng@outlook.com>
2023-11-25 00:54:15 +0800

    services/nextcloud: update

--- nixos/profiles/services/nextcloud/default.nix
+++ nixos/profiles/services/nextcloud/default.nix
@@ -8,6 +8,23 @@
   version = 27;
   package = pkgs."nextcloud${toString version}";
   inherit (package.packages) apps;
+
+  exifRename = pkgs.writeShellApplication {
+    name = "exif-rename";
+    runtimeInputs = with pkgs; [
+      exiftool
+    ];
+    text = ''
+      exiftool \
+        -fileOrder DateTimeOriginal \
+        -recurse \
+        -ignoreMinorErrors \
+        '-FileName<CreateDate' \
+        -d %Y/%m/%Y-%m-%dT%H:%M:%S%%-.3c.%%e \
+        -api LargeFileSupport=1 \
+        "$@"
+    '';
+  };
 in {
   services.nextcloud = {
     enable = true;
@@ -38,7 +55,14 @@ in {
     };
     secretFile = config.sops.templates."nextcloud-secret-config".path;
     extraApps = {
-      inherit (apps) notify_push;
+      inherit
+        (apps)
+        notify_push
+        onlyoffice
+        memories
+        previewgenerator
+        maps
+        ;
     };
     notify_push = {
       enable = true;
@@ -52,6 +76,11 @@ in {
     };
     owner = "nextcloud";
   };
+  environment.systemPackages = with pkgs; [
+    exifRename
+    ffmpeg
+    exiftool
+  ];
   services.nginx.virtualHosts.${cfg.hostName} = {
     forceSSL = true;
     useACMEHost = "main";
@@ -60,6 +89,9 @@ in {
   services.restic.backups.b2.paths = [cfg.home];
   services.restic.backups.minio.paths = [cfg.home];
 
+  systemd.services.nextcloud-cron.path = with pkgs; [
+    perl
+  ];
   systemd.services.nextcloud-notify_push = let
     nextcloudUrl = "https://nextcloud.li7g.com:${toString config.ports.https-alternative}";
   in {
@@ -68,6 +100,21 @@ in {
     environment = {
       NEXTCLOUD_URL = lib.mkForce nextcloudUrl;
     };
+  };
+  systemd.services.nextcloud-preview-generate = {
+    serviceConfig = {
+      ExecStart = "${lib.getExe cfg.occ} preview:generate-all -vvv";
+      ExecCondition = "${lib.getExe cfg.occ} status --exit-code";
+      Type = "oneshot";
+      User = "nextcloud";
+      Group = "nextcloud";
+    };
+  };
+  systemd.timers.nextcloud-preview-generate = {
+    timerConfig = {
+      OnCalendar = "*:0/5";
+    };
+    wantedBy = ["timers.target"];
   };
 
   sops.secrets."nextcloud_admin_password" = {
