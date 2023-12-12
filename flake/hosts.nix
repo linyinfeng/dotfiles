69735e24bdfb762da8256b2bbc0fa0133bab7782
Lin Yinfeng <lin.yinfeng@outlook.com>
2023-12-13 00:54:11 +0800

    hosts: replace pr 242453 with pr 263762 in comment

--- flake/hosts.nix
+++ flake/hosts.nix
@@ -238,7 +238,7 @@
       }
 
       # fixes
-      # TODO wait for https://nixpk.gs/pr-tracker.html?pr=242453
+      # TODO wait for https://nixpk.gs/pr-tracker.html?pr=263762
       ({modulesPath, ...}: {
         disabledModules = [
           "${modulesPath}/config/iproute2.nix"
