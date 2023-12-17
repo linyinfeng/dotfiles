494df3485409cb01caea5e5b969beb4233d95f8d
Lin Yinfeng <lin.yinfeng@outlook.com>
2023-12-17 23:57:27 +0800

    hosts/framework: use systemd.link to manage mobile nixos network devices

--- nixos/hosts/framework/default.nix
+++ nixos/hosts/framework/default.nix
@@ -187,11 +187,22 @@ in {
 
     # enchilada usb network
     {
-      systemd.network.networks."80-mobile-nixos-usb" = {
+      systemd.network.links."80-mobile-nixos-usb" = {
         matchConfig = {
           Property = [
             "ID_USB_VENDOR=Mobile_NixOS"
           ];
+        };
+        linkConfig = {
+          Name = "mobile0";
+        };
+      };
+      networking.networkmanager.unmanaged = [
+        "mobile0"
+      ];
+      systemd.network.networks."80-mobile-nixos-usb" = {
+        matchConfig = {
+          Name = "mobile*";
         };
         address = ["172.16.42.2/24"];
         linkConfig = {
