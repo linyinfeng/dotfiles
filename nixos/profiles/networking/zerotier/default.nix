5d7c2aefe830d00f023b47e2812fe163a58022ad
Lin Yinfeng <lin.yinfeng@outlook.com>
2023-12-21 14:30:16 +0800

    networking/zerotier: remove the localConf option
    The option is already present in upstream.

--- nixos/profiles/networking/zerotier/default.nix
+++ nixos/profiles/networking/zerotier/default.nix
@@ -1,10 +1,4 @@
-{
-  config,
-  lib,
-  pkgs,
-  ...
-}: let
-  cfg = config.services.zerotierone;
+{config, ...}: let
   stateDir = "/var/lib/zerotier-one";
   interfaceName = "zt0";
   port = config.ports.zerotier;
@@ -12,103 +6,82 @@
     "zerotierone-presetup.service"
     "zerotierone.service"
   ];
-  json = pkgs.formats.json {};
-  localConfFile = json.generate "local.conf" cfg.localConf;
 in {
-  options = {
-    services.zerotierone.localConf = lib.mkOption {
-      type = lib.types.submodule {
-        freeformType = json.type;
-        options = {
-          settings.interfacePrefixBlacklist = lib.mkOption {
-            type = with lib.types; listOf str;
-            default = [];
-          };
-        };
-      };
-      default = {};
-    };
+  services.zerotierone = {
+    enable = true;
+    inherit port;
   };
-  config = {
-    services.zerotierone = {
-      enable = true;
-      inherit port;
+  passthru.zerotierInterfaceName = interfaceName;
+  systemd.services.zerotierone-presetup = {
+    script = ''
+      echo "create directory"
+      mkdir -p "${stateDir}"
+
+      echo "setting up identity files..."
+      cp "${config.sops.secrets."zerotier_public_key".path}" "${stateDir}/identity.public"
+      cp "${config.sops.secrets."zerotier_private_key".path}" "${stateDir}/identity.secret"
+
+      echo "setting up network interface..."
+      mkdir -p "${stateDir}/networks.d"
+      NETWORK_ID=$(cat "${config.sops.secrets."zerotier_network_id".path}")
+      touch "${stateDir}/networks.d/$NETWORK_ID.conf"
+      echo "$NETWORK_ID=${interfaceName}" > "${stateDir}/devicemap"
+
+      echo "cleaning up moon..."
+      rm -rf "${stateDir}/moons.d"
+
+      echo "setting up moon..."
+      mkdir -p "${stateDir}/moons.d"
+      FILENAME=$(cat ${config.sops.secrets."zerotier_moon/filename".path})
+      cat ${config.sops.secrets."zerotier_moon/content_base64".path} |\
+        base64 --decode \
+        > "${stateDir}/moons.d/$FILENAME"
+    '';
+    serviceConfig = {
+      Type = "oneshot";
+      RemainAfterExit = true;
     };
-    passthru.zerotierInterfaceName = interfaceName;
-    systemd.services.zerotierone-presetup = {
-      script = ''
-        echo "create directory"
-        mkdir -p "${stateDir}"
-
-        echo "setting up identity files..."
-        cp "${config.sops.secrets."zerotier_public_key".path}" "${stateDir}/identity.public"
-        cp "${config.sops.secrets."zerotier_private_key".path}" "${stateDir}/identity.secret"
-
-        echo "setting up network interface..."
-        mkdir -p "${stateDir}/networks.d"
-        NETWORK_ID=$(cat "${config.sops.secrets."zerotier_network_id".path}")
-        touch "${stateDir}/networks.d/$NETWORK_ID.conf"
-        echo "$NETWORK_ID=${interfaceName}" > "${stateDir}/devicemap"
-
-        echo "cleaning up moon..."
-        rm -rf "${stateDir}/moons.d"
-
-        echo "setting up moon..."
-        mkdir -p "${stateDir}/moons.d"
-        FILENAME=$(cat ${config.sops.secrets."zerotier_moon/filename".path})
-        cat ${config.sops.secrets."zerotier_moon/content_base64".path} |\
-          base64 --decode \
-          > "${stateDir}/moons.d/$FILENAME"
-
-        echo "setting up local.conf..."
-        ln -sf "${localConfFile}" "${stateDir}/local.conf"
-      '';
-      serviceConfig = {
-        Type = "oneshot";
-        RemainAfterExit = true;
-      };
-      before = ["zerotierone.service"];
-      wantedBy = ["multi-user.target"];
-    };
-    systemd.services.zerotierone.requires = [
-      "zerotierone-presetup.service"
-    ];
-    sops.secrets."zerotier_network_id" = {
-      sopsFile = config.sops-file.get "terraform/infrastructure.yaml";
-      restartUnits = units;
-    };
-    sops.secrets."zerotier_moon/filename" = {
-      sopsFile = config.sops-file.get "terraform/infrastructure.yaml";
-      restartUnits = units;
-    };
-    sops.secrets."zerotier_moon/content_base64" = {
-      sopsFile = config.sops-file.get "terraform/infrastructure.yaml";
-      restartUnits = units;
-    };
-    sops.secrets."zerotier_public_key" = {
-      sopsFile = config.sops-file.terraform;
-      restartUnits = units;
-    };
-    sops.secrets."zerotier_private_key" = {
-      sopsFile = config.sops-file.terraform;
-      restartUnits = units;
-    };
-
-    networking.firewall.allowedUDPPorts = [
-      config.services.zerotierone.port
-    ];
-    networking.firewall.allowedTCPPorts = [
-      config.services.zerotierone.port
-    ];
-
-    services.zerotierone.localConf.settings.interfacePrefixBlacklist = [
-      "tailscale"
-      # mesh interfaces
-      "mesh"
-      "dn42"
-      "as198764"
-    ];
-
-    networking.networkmanager.unmanaged = [interfaceName];
+    before = ["zerotierone.service"];
+    wantedBy = ["multi-user.target"];
   };
+  systemd.services.zerotierone.requires = [
+    "zerotierone-presetup.service"
+  ];
+  sops.secrets."zerotier_network_id" = {
+    sopsFile = config.sops-file.get "terraform/infrastructure.yaml";
+    restartUnits = units;
+  };
+  sops.secrets."zerotier_moon/filename" = {
+    sopsFile = config.sops-file.get "terraform/infrastructure.yaml";
+    restartUnits = units;
+  };
+  sops.secrets."zerotier_moon/content_base64" = {
+    sopsFile = config.sops-file.get "terraform/infrastructure.yaml";
+    restartUnits = units;
+  };
+  sops.secrets."zerotier_public_key" = {
+    sopsFile = config.sops-file.terraform;
+    restartUnits = units;
+  };
+  sops.secrets."zerotier_private_key" = {
+    sopsFile = config.sops-file.terraform;
+    restartUnits = units;
+  };
+
+  networking.firewall.allowedUDPPorts = [
+    config.services.zerotierone.port
+  ];
+  networking.firewall.allowedTCPPorts = [
+    config.services.zerotierone.port
+  ];
+
+  services.zerotierone.localConf.settings.interfacePrefixBlacklist = [
+    "tailscale"
+    # mesh interfaces
+    "mesh"
+    "dn42"
+    "as198764"
+  ];
+
+  networking.networkmanager.unmanaged = [interfaceName];
 }
