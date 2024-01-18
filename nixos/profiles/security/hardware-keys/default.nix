89fcef45c372ddf763431d010167c6200b086cc0
Lin Yinfeng <lin.yinfeng@outlook.com>
2024-01-18 13:43:46 +0800

    security/hardware-keys/pcscd: add polkit workaround

--- nixos/profiles/security/hardware-keys/default.nix
+++ nixos/profiles/security/hardware-keys/default.nix
@@ -16,6 +16,9 @@
   environment.systemPackages = with pkgs; [
     yubikey-manager
     pcsctools
+
+    # TODO wait for https://nixpk.gs/pr-tracker.html?pr=281421
+    pcscliteWithPolkit.out
   ];
 
   # disabled
