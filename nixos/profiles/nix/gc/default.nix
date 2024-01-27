308aaf1d8c9402a22ca13022e729a2fd415693e8
Lin Yinfeng <lin.yinfeng@outlook.com>
2024-01-27 15:06:11 +0800

    nix/gc: delete more profiles in gc

--- nixos/profiles/nix/gc/default.nix
+++ nixos/profiles/nix/gc/default.nix
@@ -1,13 +1,15 @@
 {
-  pkgs,
+  config,
   lib,
   ...
 }: {
   nix.gc = {
     automatic = true;
-    options = lib.mkDefault ''
-      --delete-older-than 14d
-    '';
+    options = lib.mkDefault (
+      if config.system.types == ["server"]
+      then "--delete-old"
+      else "--delete-older-than 2d"
+    );
   };
   nix.settings.min-free = 1024 * 1024 * 1024; # bytes
 }
