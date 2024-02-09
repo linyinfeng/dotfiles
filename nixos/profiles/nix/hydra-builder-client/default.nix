3d5a9bce39a3d99683f708d6d691986f96d99744
Lin Yinfeng <lin.yinfeng@outlook.com>
2024-02-09 23:03:35 +0800

    nix/hydra-builder-client: build more packages on nuc

--- nixos/profiles/nix/hydra-builder-client/default.nix
+++ nixos/profiles/nix/hydra-builder-client/default.nix
@@ -10,10 +10,9 @@ in {
   };
   # https://nixos.org/manual/nix/stable/advanced-topics/distributed-builds
   environment.etc."${dir}/machines".text = ''
-    # only build on nuc if big-parallel
-    hydra-builder@nuc  x86_64-linux,i686-linux               /etc/${keyFile} 8 1 kvm,nixos-test,benchmark,big-parallel big-parallel
-    hydra-builder@hil0 x86_64-linux,i686-linux               /etc/${keyFile} 2 1
-    hydra-builder@fsn0 aarch64-linux                         /etc/${keyFile} 2 1 benchmark,big-parallel
+    hydra-builder@nuc  x86_64-linux,i686-linux,aarch64-linux /etc/${keyFile} 8 100 kvm,nixos-test,benchmark,big-parallel
+    hydra-builder@hil0 x86_64-linux,i686-linux               /etc/${keyFile} 2 100
+    hydra-builder@fsn0 aarch64-linux                         /etc/${keyFile} 2 100
   '';
   environment.etc."${dir}/machines-workstation".text = ''
     hydra-builder@nuc       x86_64-linux,i686-linux /etc/${keyFile} 8 100 kvm,nixos-test,benchmark,big-parallel
