1ac9404a7f4ee2eb788bc76107a4c851d55ad0f2
Lin Yinfeng <lin.yinfeng@outlook.com>
2024-02-13 22:18:38 +0800

    nix/hydra-builder-clinet: adjust speed factors

--- nixos/profiles/nix/hydra-builder-client/default.nix
+++ nixos/profiles/nix/hydra-builder-client/default.nix
@@ -11,18 +11,17 @@ in {
   # https://nixos.org/manual/nix/stable/advanced-topics/distributed-builds
   environment.etc."${dir}/machines".text = ''
     hydra-builder@nuc  x86_64-linux,i686-linux,aarch64-linux /etc/${keyFile} 8 100 kvm,nixos-test,benchmark,big-parallel
-    hydra-builder@fsn0 aarch64-linux                         /etc/${keyFile} 2 100
+    hydra-builder@fsn0 aarch64-linux                         /etc/${keyFile} 2 50
   '';
   environment.etc."${dir}/machines-workstation".text = ''
     hydra-builder@nuc       x86_64-linux,i686-linux /etc/${keyFile} 8 100 kvm,nixos-test,benchmark,big-parallel
     hydra-builder@xps8930   x86_64-linux,i686-linux /etc/${keyFile} 8 100 kvm,nixos-test,benchmark,big-parallel
     hydra-builder@framework x86_64-linux,i686-linux /etc/${keyFile} 8 100 kvm,nixos-test,benchmark,big-parallel
-    hydra-builder@hil0      x86_64-linux,i686-linux /etc/${keyFile} 2 50
 
-    hydra-builder@fsn0      aarch64-linux /etc/${keyFile} 2 100 benchmark,big-parallel
-    hydra-builder@nuc       aarch64-linux /etc/${keyFile} 8 50  kvm,nixos-test
-    hydra-builder@xps8930   aarch64-linux /etc/${keyFile} 8 50  kvm,nixos-test
-    hydra-builder@framework aarch64-linux /etc/${keyFile} 8 50  kvm,nixos-test
+    hydra-builder@fsn0      aarch64-linux /etc/${keyFile} 2 50 benchmark,big-parallel
+    hydra-builder@nuc       aarch64-linux /etc/${keyFile} 8 100 kvm,nixos-test
+    hydra-builder@xps8930   aarch64-linux /etc/${keyFile} 8 100 kvm,nixos-test
+    hydra-builder@framework aarch64-linux /etc/${keyFile} 8 100 kvm,nixos-test
   '';
   sops.secrets."hydra_builder_private_key" = {
     neededForUsers = true; # needed for /etc
