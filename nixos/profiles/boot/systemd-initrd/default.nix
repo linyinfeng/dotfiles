d230259a1ae2bd234b35e9e80e39ff1f84feb734
Lin Yinfeng <lin.yinfeng@outlook.com>
2024-03-08 16:32:01 +0800

    boot/systemd-initrd: disable gpt-auto by default

--- nixos/profiles/boot/systemd-initrd/default.nix
+++ nixos/profiles/boot/systemd-initrd/default.nix
@@ -1,8 +1,18 @@
-{...}: {
-  boot.initrd.systemd = {
-    enable = true;
-    emergencyAccess = true;
+{
+  config,
+  lib,
+  ...
+}: {
+  options = {
+    boot.initrd.systemd.gpt-auto.enable = lib.mkEnableOption "gpt-auto";
   };
-  # enable systemd EFI support in initrd
-  boot.initrd.availableKernelModules = ["efivarfs"];
+  config = {
+    boot.initrd.systemd = {
+      enable = true;
+      emergencyAccess = true;
+    };
+    # enable systemd EFI support in initrd
+    boot.initrd.availableKernelModules = ["efivarfs"];
+    boot.kernelParams = lib.mkIf (!config.boot.initrd.systemd.gpt-auto.enable) ["rd.systemd.gpt_auto=0"];
+  };
 }
