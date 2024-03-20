0285215a69d195231eb0e75918e68dc5d67b33c3
Lin Yinfeng <lin.yinfeng@outlook.com>
2024-03-20 10:48:25 +0800

    boot/systemd-initrd: fix

--- nixos/profiles/boot/systemd-initrd/default.nix
+++ nixos/profiles/boot/systemd-initrd/default.nix
@@ -1,21 +1,7 @@
 { config, lib, ... }:
 {
-  options = {
-    # TODO wait for https://nixpk.gs/pr-tracker.html?pr=282022
-    boot.initrd.systemd.root = lib.mkOption {
-      type = lib.types.str;
-      default = "fstab";
-    };
-  };
-  config = {
-    boot.initrd.systemd = {
-      enable = true;
-      emergencyAccess = true;
-    };
-
-    # TODO wait for https://nixpk.gs/pr-tracker.html?pr=282022
-    # enable systemd EFI support in initrd
-    boot.initrd.availableKernelModules = [ "efivarfs" ];
-    boot.kernelParams = [ "root=${config.boot.initrd.systemd.root}" ];
+  boot.initrd.systemd = {
+    enable = true;
+    emergencyAccess = true;
   };
 }
