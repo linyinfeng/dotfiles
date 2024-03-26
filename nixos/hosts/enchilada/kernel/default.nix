7746957bbfe498f0184a64b509af9986bc1b870e
Lin Yinfeng <lin.yinfeng@outlook.com>
2024-03-26 20:37:17 +0800

    hosts/enchilada/kernel: update

--- nixos/hosts/enchilada/kernel/default.nix
+++ nixos/hosts/enchilada/kernel/default.nix
@@ -1,13 +1,13 @@
 { mobile-nixos, fetchFromGitLab, ... }:
 mobile-nixos.kernel-builder {
-  version = "6.6.3";
+  version = "6.7.8";
   configfile = ./config.aarch64;
 
   src = fetchFromGitLab {
     owner = "sdm845-mainline";
     repo = "linux";
-    rev = "sdm845-6.6.3-r3";
-    hash = "sha256-StE6pFwSPklhI0xjp85JSPG0yIFOZ6VU72mQoVrIFSo=";
+    rev = "sdm845-6.7.8";
+    hash = "sha256-vefydDqyGMs1w2I8RIitzEJ/V+MiBCqQMGECwxjixD4=";
   };
 
   patches = [ ];
