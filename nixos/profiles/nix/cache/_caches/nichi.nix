03cc3a0230af8d8a7a9d92266fa65da3da588efe
Lin Yinfeng <lin.yinfeng@outlook.com>
2024-05-05 13:31:27 +0800

    nix/cache: decrease priority for cache.nichi.co

--- nixos/profiles/nix/cache/_caches/nichi.nix
+++ nixos/profiles/nix/cache/_caches/nichi.nix
@@ -1,6 +1,6 @@
 {
   nix.settings = {
-    substituters = [ "https://cache.nichi.co" ];
+    substituters = [ "https://cache.nichi.co?priority=50" ];
     trusted-public-keys = [ "hydra.nichi.co-0:P3nkYHhmcLR3eNJgOAnHDjmQLkfqheGyhZ6GLrUVHwk=" ];
   };
 }
