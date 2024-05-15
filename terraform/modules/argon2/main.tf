eb0bbbbcaf6f5da4d26f38973f9393380d4f817e
Lin Yinfeng <lin.yinfeng@outlook.com>
2024-05-15 12:06:02 +0800

    terraform/modules/argon2: fix

--- terraform/modules/argon2/main.tf
+++ terraform/modules/argon2/main.tf
@@ -10,7 +10,7 @@ resource "shell_sensitive_script" "argon2" {
   lifecycle_commands {
     create = <<EOT
       set -e
-      HASHED=$(echo $PASSWORD | argon2 "$SALT" -id -e)
+      HASHED=$(echo -n "$PASSWORD" | argon2 "$SALT" -id -e)
       jq --null-input \
         --arg hashed "$HASHED" \
         '{"hashed": $hashed}'
