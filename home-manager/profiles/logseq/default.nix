ca99d4130b69b56ee274ec86a881dac7f9bfb7b5
Lin Yinfeng <lin.yinfeng@outlook.com>
2024-05-17 16:25:13 +0800

    hm/logseq: enable wayland ime

--- home-manager/profiles/logseq/default.nix
+++ home-manager/profiles/logseq/default.nix
@@ -2,6 +2,17 @@
 
 {
   home.packages = with pkgs; [ logseq ];
+  xdg.desktopEntries = {
+    logseq = {
+      name = "Logseq";
+      exec = "logseq --enable-wayland-ime %U";
+      icon = "logseq";
+      settings.StartupWMClass = "Logseq";
+      comment = "A privacy-first, open-source platform for knowledge management and collaboration.";
+      mimeType = [ "x-scheme-handler/logseq" ];
+      categories = [ "Utility" ];
+    };
+  };
   dconf.settings."org/gnome/shell".favorite-apps = lib.mkAfter [ "logseq.desktop" ];
   home.global-persistence.directories = [
     ".config/Logseq"
