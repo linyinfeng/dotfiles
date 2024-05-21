aa31969a880617108a51806ca17afe471b3528c1
Lin Yinfeng <lin.yinfeng@outlook.com>
2024-05-21 11:23:29 +0800

    graphical/gnome: update PAM service

--- nixos/profiles/graphical/gnome/default.nix
+++ nixos/profiles/graphical/gnome/default.nix
@@ -118,31 +118,23 @@ lib.mkIf config.services.xserver.desktopManager.gnome.enable {
   };
 
   security.pam.services = {
-    gdm-password.text = lib.mkForce ''
-      auth     requisite      pam_nologin.so
-      auth     required       pam_succeed_if.so uid >= 1000 quiet
-      auth     optional       pam_unix.so nullok likeauth
-      auth     optional       ${pkgs.gnome.gnome-keyring}/lib/security/pam_gnome_keyring.so
-      auth     sufficient     pam_unix.so nullok likeauth try_first_pass
-      auth     required       pam_deny.so
-
-      account  include        login
-      password substack       login
-
-      session  optional       pam_keyinit.so revoke
-      session  include        login
-    '';
+    login.fprintAuth = false;
     gdm-fingerprint.text = ''
-      auth     requisite      pam_nologin.so
-      auth     required       pam_succeed_if.so uid >= 1000 quiet
-      auth     required       ${pkgs.fprintd}/lib/security/pam_fprintd.so
-      auth     optional       ${pkgs.gnome.gnome-keyring}/lib/security/pam_gnome_keyring.so
+      auth       required                    pam_shells.so
+      auth       requisite                   pam_nologin.so
+      auth       requisite                   pam_faillock.so      preauth
+      auth       required                    ${pkgs.fprintd}/lib/security/pam_fprintd.so
+      auth       optional                    pam_permit.so
+      auth       required                    pam_env.so
+      auth       [success=ok default=1]      ${pkgs.gnome.gdm}/lib/security/pam_gdm.so
+      auth       optional                    ${pkgs.gnome.gnome-keyring}/lib/security/pam_gnome_keyring.so
 
-      account  include        login
-      password required       ${pkgs.fprintd}/lib/security/pam_fprintd.so
+      account    include                     login
 
-      session  optional       pam_keyinit.so revoke
-      session  include        login
+      password   required                    pam_deny.so
+
+      session    include                     login
+      session    optional                    pam_gnome_keyring.so auto_start
     '';
   };
 }
