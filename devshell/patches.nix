88bdee883ce9271b89ab5200e5853d382f66d558
Lin Yinfeng <lin.yinfeng@outlook.com>
2024-06-10 00:55:02 +0800

    patches: disable auto update of mutter-text-input-v1.patch

--- devshell/patches.nix
+++ devshell/patches.nix
@@ -4,7 +4,8 @@ let
     name = "update-patches";
     text = ''
       cd "$PRJ_ROOT/patches"
-      curl https://gitlab.gnome.org/GNOME/mutter/-/merge_requests/3751.patch > mutter-text-input-v1.patch
+      # TODO broken on newest commits
+      # curl https://gitlab.gnome.org/GNOME/mutter/-/merge_requests/3751.patch > mutter-text-input-v1.patch
       curl https://gitlab.gnome.org/GNOME/gnome-shell/-/merge_requests/3318.patch > gnome-shell-preedit-fix.patch
     '';
   };
