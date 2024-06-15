ba5c7592664569057dccb25bcdba96b941fdf14d
Lin Yinfeng <lin.yinfeng@outlook.com>
2024-06-15 13:26:20 +0800

    services/ace-bot: update

--- nixos/profiles/services/ace-bot/default.nix
+++ nixos/profiles/services/ace-bot/default.nix
@@ -1,21 +1,28 @@
-{ config, pkgs, ... }:
+{ config, ... }:
 {
   services.ace-bot = {
     enable = true;
     managerChatId = "148111617";
     tokenFile = config.sops.secrets."telegram-bot/ace-bot/token".path;
-    packages =
-      with pkgs;
-      [
-        coreutils
-        util-linux
-        procps
-        curlFull
-        wget
-        texlive.combined.scheme-full
-        python3Full
-      ]
-      ++ [ config.nix.package ];
+    extraModules = [
+      (
+        { pkgs, ... }:
+        {
+          nix.registry = {
+            inherit (config.nix.registry) nixpkgs;
+          };
+          nix.nixPath = [ "nixpkgs=${pkgs.path}" ];
+          environment.systemPackages = with pkgs; [
+            util-linux
+            procps
+            curlFull
+            wget
+            texlive.combined.scheme-full
+            python3Full
+          ];
+        }
+      )
+    ];
   };
   sops.secrets."telegram-bot/ace-bot/token" = {
     sopsFile = config.sops-file.host;
@@ -23,7 +30,7 @@
   services.nginx.virtualHosts."ace-bot.*" = {
     forceSSL = true;
     inherit (config.security.acme.tfCerts."li7g_com".nginxSettings) sslCertificate sslCertificateKey;
-    locations."/".root = "/var/lib/ace-bot/home";
+    locations."/".root = "/var/lib/ace-bot/mount";
     extraConfig = ''
       index =404;
       autoindex on;
