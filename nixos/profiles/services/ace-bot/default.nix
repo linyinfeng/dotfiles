850f9bffd4d38c4519a73b4076c0a12b597716e1
Lin Yinfeng <lin.yinfeng@outlook.com>
2024-06-25 18:19:02 +0800

    services/ace-bot: update

--- nixos/profiles/services/ace-bot/default.nix
+++ nixos/profiles/services/ace-bot/default.nix
@@ -1,4 +1,9 @@
-{ config, ... }:
+{
+  config,
+  pkgs,
+  lib,
+  ...
+}:
 {
   services.ace-bot = {
     enable = true;
@@ -24,16 +29,26 @@
       )
     ];
   };
+  systemd.slices.ace-bot = {
+    sliceConfig = {
+      CPUQuota = "100%";
+    };
+  };
   sops.secrets."telegram-bot/ace-bot/token" = {
     sopsFile = config.sops-file.host;
   };
+  services.nginx.additionalModules = with pkgs.nginxModules; [ fancyindex ];
   services.nginx.virtualHosts."ace-bot.*" = {
     forceSSL = true;
     inherit (config.security.acme.tfCerts."li7g_com".nginxSettings) sslCertificate sslCertificateKey;
-    locations."/".root = "/var/lib/ace-bot/mount";
+    locations."/".root = "/var/lib/ace-bot/mount/disk";
     extraConfig = ''
       index =404;
-      autoindex on;
+      fancyindex on;
+      fancyindex_show_dotfiles on;
+      fancyindex_exact_size off;
+      fancyindex_localtime on;
+      fancyindex_hide_parent_dir on;
     '';
   };
   users.users.nginx.extraGroups = [ config.users.groups.ace-bot.name ];
