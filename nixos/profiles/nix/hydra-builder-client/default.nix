7f4085d47f8e1a7a35dc175cf072d637a6f338c7
Lin Yinfeng <lin.yinfeng@outlook.com>
2024-07-02 13:13:22 +0800

    nix/hydra-builder-client: update machine files

--- nixos/profiles/nix/hydra-builder-client/default.nix
+++ nixos/profiles/nix/hydra-builder-client/default.nix
@@ -13,17 +13,17 @@ in
   # https://nixos.org/manual/nix/stable/advanced-topics/distributed-builds
   environment.etc."${dir}/machines".text = ''
     hydra-builder@nuc  x86_64-linux,i686-linux,aarch64-linux /etc/${keyFile} 8 100 kvm,nixos-test,benchmark,big-parallel
-    hydra-builder@fsn0 aarch64-linux                         /etc/${keyFile} 2 50
+    hydra-builder@fsn0 aarch64-linux                         /etc/${keyFile} 2 200 benchmark,big-parallel
   '';
   environment.etc."${dir}/machines-workstation".text = ''
     hydra-builder@nuc       x86_64-linux,i686-linux /etc/${keyFile} 8 100 kvm,nixos-test,benchmark,big-parallel
     hydra-builder@xps8930   x86_64-linux,i686-linux /etc/${keyFile} 8 100 kvm,nixos-test,benchmark,big-parallel
     hydra-builder@framework x86_64-linux,i686-linux /etc/${keyFile} 8 100 kvm,nixos-test,benchmark,big-parallel
 
-    hydra-builder@fsn0      aarch64-linux /etc/${keyFile} 2 50 benchmark,big-parallel
-    hydra-builder@nuc       aarch64-linux /etc/${keyFile} 8 100 kvm,nixos-test
-    hydra-builder@xps8930   aarch64-linux /etc/${keyFile} 8 100 kvm,nixos-test
-    hydra-builder@framework aarch64-linux /etc/${keyFile} 8 100 kvm,nixos-test
+    hydra-builder@fsn0      aarch64-linux /etc/${keyFile} 2 100 benchmark,big-parallel
+    hydra-builder@nuc       aarch64-linux /etc/${keyFile} 8 50
+    hydra-builder@xps8930   aarch64-linux /etc/${keyFile} 8 50
+    hydra-builder@framework aarch64-linux /etc/${keyFile} 8 50
   '';
   sops.secrets."hydra_builder_private_key" = {
     neededForUsers = true; # needed for /etc
