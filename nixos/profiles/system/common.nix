d23a8c5e1b0ac91289d24dd5b4d862164d784d8e
Lin Yinfeng <lin.yinfeng@outlook.com>
2024-08-09 15:11:42 +0800

    system/common: enable etc overlay

--- nixos/profiles/system/common.nix
+++ nixos/profiles/system/common.nix
@@ -4,5 +4,8 @@
   console.keyMap = lib.mkDefault "us";
   time.timeZone = lib.mkDefault "Asia/Shanghai";
 
+  system.etc.overlay.enable = true;
+  # TODO wait for sops-nix support
+  # services.userborn.enable = true;
   users.mutableUsers = lib.mkDefault false;
 }
