00fc520e40afb162b52e8e54a55878350d2116e7
Lin Yinfeng <lin.yinfeng@outlook.com>
2024-08-26 17:17:02 +0800

    hosts/framework: fix nvidia

--- nixos/hosts/framework/_nvidia.nix
+++ nixos/hosts/framework/_nvidia.nix
@@ -1,9 +1,29 @@
-{ ... }:
+{ config, ... }:
+let
+  originalPackage = config.boot.kernelPackages.nvidiaPackages.stable;
+  package = originalPackage // {
+    open = originalPackage.open.overrideAttrs (old: {
+      nativeBuildInputs = (old.nativeBuildInputs or [ ]) ++ [
+        config.boot.kernelModuleSigning.signModule
+      ];
+      # signature will be stripped
+      dontStrip = true;
+      postBuild =
+        (old.postBuild or "")
+        + ''
+          for module in kernel-open/*.ko; do
+            sign-module "$module"
+          done
+        '';
+    });
+  };
+in
 {
   services.xserver.videoDrivers = [ "nvidia" ];
   hardware.nvidia = {
     open = true;
     modesetting.enable = true;
     nvidiaSettings = true;
+    inherit package;
   };
 }
