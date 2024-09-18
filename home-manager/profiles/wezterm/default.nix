badc2cb8ba0d51b1d084b76dbc83af9453e6eb28
Lin Yinfeng <lin.yinfeng@outlook.com>
2024-09-19 02:08:11 +0800

    hm/wezterm: init (not enabled)

---
+++ home-manager/profiles/wezterm/default.nix
@@ -0,0 +1,58 @@
+{ pkgs, lib, ... }:
+let
+  themeFile = ".config/wezterm/theme.lua";
+  darkmanSwitch = pkgs.writeShellApplication {
+    name = "darkman-switch-wezterm";
+    text = ''
+      mode="$1"
+      if [ "$mode" = light ]; then
+        theme="Builtin Tango Light"
+      elif [ "$mode" = dark ]; then
+        theme="Builtin Tango Dark"
+      else
+        echo "invalid mode: $mode"
+        exit 1
+      fi
+      cat >~/"${themeFile}" <<EOF
+        return "$theme"
+      EOF
+    '';
+  };
+in
+{
+  programs.wezterm = {
+    enable = true;
+    extraConfig = ''
+      local config = wezterm.config_builder()
+
+      function get_appearance()
+        if wezterm.gui then
+          return wezterm.gui.get_appearance()
+        end
+        return 'Dark'
+      end
+
+      function scheme_for_appearance(appearance)
+        if appearance:find 'Dark' then
+          return 'Builtin Tango Dark'
+        else
+          return 'Builtin Tango Light'
+        end
+      end
+
+      config.color_scheme = scheme_for_appearance(get_appearance())
+      config.font = wezterm.font('monospace')
+      -- TODO wait for https://github.com/wez/wezterm/issues/5990
+      config.front_end = 'WebGpu'
+      return config
+    '';
+  };
+  systemd.user.tmpfiles.rules = [
+    # create theme file if not exists
+    ''f %h/${themeFile} - - - - return "Builtin Tango Light"''
+  ];
+  services.darkman = {
+    lightModeScripts.wezterm = "${lib.getExe darkmanSwitch} light";
+    darkModeScripts.wezterm = "${lib.getExe darkmanSwitch} dark";
+  };
+}
