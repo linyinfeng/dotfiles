9cb112b15cdea69c908f3e3a8dd6ec14303acb38
Lin Yinfeng <lin.yinfeng@outlook.com>
2024-09-20 21:25:31 +0800

    hm/development: add racket

--- home-manager/profiles/development/default.nix
+++ home-manager/profiles/development/default.nix
@@ -1,10 +1,17 @@
-{ ... }:
+{ pkgs, ... }:
 {
+  home.packages = with pkgs; [
+    racket
+  ];
+
   home.global-persistence.directories = [
     ".rustup"
     ".cargo"
     ".cabal"
     ".wrangler"
+
+    ".config/racket"
+    ".local/share/racket"
 
     "Source"
     "Local"
