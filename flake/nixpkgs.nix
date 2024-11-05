eb03e355ab38126d5d390c6e6d9ce92da59f3b00
Lin Yinfeng <lin.yinfeng@outlook.com>
2024-11-05 18:36:59 +0800

    nixpkgs: add workaround for jellyfin

--- flake/nixpkgs.nix
+++ flake/nixpkgs.nix
@@ -169,6 +169,8 @@ let
         # TODO wait for https://nixpkgs-tracker.ocfox.me/?pr=
         # TODO wait for https://nixpkgs-tracker.ocfox.me/?pr=353230
         inherit (unstable-small) yubikey-manager;
+        # TODO wait for https://nixpkgs-tracker.ocfox.me/?pr=353198
+        inherit (unstable-smalle) jellyfin-ffmpeg;
       })
     ];
 in
