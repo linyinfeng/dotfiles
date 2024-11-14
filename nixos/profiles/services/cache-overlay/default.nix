d770100ec70a2f7c621e7155b37dece1550a25ca
Lin Yinfeng <lin.yinfeng@outlook.com>
2024-11-15 02:24:25 +0800

    services/cache-overlay: disable verbose logging for aws-s3-reverse-proxy

--- nixos/profiles/services/cache-overlay/default.nix
+++ nixos/profiles/services/cache-overlay/default.nix
@@ -59,7 +59,6 @@ in
       export AWS_CREDENTIALS="$AWS_ACCESS_KEY_ID,$AWS_SECRET_ACCESS_KEY"
 
       ${pkgs.nur.repos.linyinfeng.aws-s3-reverse-proxy}/bin/aws-s3-reverse-proxy \
-        --verbose \
         --allowed-endpoint="cache-overlay.ts.li7g.com" \
         --listen-addr=":${toString sigv4ProxyPort}" \
         --allowed-source-subnet=127.0.0.1/8 \
