95a767e166650dfa3139d637626e5464bfe1d93d
Lin Yinfeng <lin.yinfeng@outlook.com>
2024-12-02 14:26:07 +0800

    hosts/owl: enable boost

--- nixos/hosts/owl/default.nix
+++ nixos/hosts/owl/default.nix
@@ -59,10 +59,8 @@
       services.tlp = {
         enable = true;
         settings = {
-          CPU_BOOST_ON_BAT = 0;
           CPU_ENERGY_PERF_POLICY_ON_BAT = "power";
           PLATFORM_PROFILE_ON_BAT = "low-power";
-          CPU_BOOST_ON_AC = 0;
           CPU_ENERGY_PERF_POLICY_ON_AC = "balance_performance";
           PLATFORM_PROFILE_ON_AC = "balanced";
         };
