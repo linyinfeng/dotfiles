757cffe58baf475148c2a9e12e46e836780b8ae4
Lin Yinfeng <lin.yinfeng@outlook.com>
2025-01-04 17:42:42 +0800

    networking/resolved: dnssec allow downgrade

--- nixos/profiles/networking/resolved/default.nix
+++ nixos/profiles/networking/resolved/default.nix
@@ -8,11 +8,7 @@ lib.mkMerge [
   {
     services.resolved = {
       enable = true;
-      # At the time of September 2023, systemd upstream advise to disable DNSSEC by default as the current code is not
-      # robust enough to deal with “in the wild” non-compliant servers, which will usually give you a broken bad
-      # experience in addition of insecure.
-      # dnssec = "allow-downgrade";
-      dnssec = "false";
+      dnssec = "allow-downgrade";
       llmnr = "true";
       extraConfig = lib.mkIf config.networking.mesh.enable ''
         DNS=${lib.concatStringsSep " " dnsServers}
