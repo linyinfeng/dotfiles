314b7c3c351d479c9882b6da91b4d41ae94d86df
Lin Yinfeng <lin.yinfeng@outlook.com>
2025-02-16 21:54:42 +0800

    hm/chromium: update commandLineArgs

--- home-manager/profiles/chromium/default.nix
+++ home-manager/profiles/chromium/default.nix
@@ -3,10 +3,18 @@
   programs.chromium = {
     enable = true;
     commandLineArgs = [
+      # web features
       "--enable-experimental-web-platform-features"
+
+      # wayland
       "--ozone-platform-hint=auto"
+
+      # input method
       "--enable-wayland-ime"
-      "--wayland-text-input-version=3" # wait for chromium 129
+      "--wayland-text-input-version=3"
+
+      # video acceleration
+      "--enable-features=AcceleratedVideoDecodeLinuxGL"
     ];
   };
 
