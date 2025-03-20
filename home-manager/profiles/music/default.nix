def378f774fc9600cd9609e8e396c87fbc30ad7b
Lin Yinfeng <lin.yinfeng@outlook.com>
2025-03-20 11:03:14 +0800

    hm/music: add neural-amp-modeler

--- home-manager/profiles/music/default.nix
+++ home-manager/profiles/music/default.nix
@@ -1,10 +1,24 @@
-{ pkgs, ... }:
-{
-  home.packages = with pkgs; [
-    reaper
-    lilypond
-    musescore
+{ config, pkgs, ... }:
+let
+  audioPlugins = with pkgs; [
+    # lv2
+    neural-amp-modeler-lv2
+    # ladspa
   ];
+in
+{
+  home.packages =
+    with pkgs;
+    [
+      reaper
+      lilypond
+      musescore
+    ]
+    ++ audioPlugins;
+  home.sessionVariables = {
+    LV2_PATH = "${config.xdg.stateHome}/nix/profiles/home-manager/home-path/lib/lv2";
+    LADSPA_PATH = "${config.xdg.stateHome}/nix/profiles/home-manager/home-path/lib/ladspa";
+  };
   home.global-persistence.directories = [
     ".config/REAPER"
     ".config/MuseScore"
