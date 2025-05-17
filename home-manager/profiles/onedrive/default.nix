344a3a40b72d04f6f9556ebc3acf63fc5695d50a
Lin Yinfeng <lin.yinfeng@outlook.com>
2025-05-18 00:33:08 +0800

    hm/onedrive: enable service

--- home-manager/profiles/onedrive/default.nix
+++ home-manager/profiles/onedrive/default.nix
@@ -1,6 +1,12 @@
 { pkgs, ... }:
 {
   home.packages = with pkgs; [ onedrive ];
+  programs.onedrive = {
+    enable = true;
+    settings = {
+      # currently nothing
+    };
+  };
 
   home.global-persistence.directories = [
     ".config/onedrive"
