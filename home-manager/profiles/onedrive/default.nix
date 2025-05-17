9b196c27ba6c0b9d144cf89f5264d5d42e2bef81
Lin Yinfeng <lin.yinfeng@outlook.com>
2025-05-18 01:06:04 +0800

    hm/onedrive: enable monitor service

--- home-manager/profiles/onedrive/default.nix
+++ home-manager/profiles/onedrive/default.nix
@@ -1,12 +1,16 @@
-{ pkgs, ... }:
+{ config, ... }:
 {
-  home.packages = with pkgs; [ onedrive ];
   programs.onedrive = {
     enable = true;
     settings = {
       # currently nothing
     };
   };
+
+  systemd.user.tmpfiles.rules = [
+    # enable onedrive service
+    "L %h/.config/systemd/user/default.target.wants/onedrive.service - - - - ${config.programs.onedrive.package}/share/systemd/user/onedrive.service"
+  ];
 
   home.global-persistence.directories = [
     ".config/onedrive"
