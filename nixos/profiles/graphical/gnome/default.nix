c5994cd537c951df0653ad2102508bd825802c58
Lin Yinfeng <lin.yinfeng@outlook.com>
2025-06-09 23:28:39 +0800

    graphical/gnome: remove gdm-fingerprint workaround

--- nixos/profiles/graphical/gnome/default.nix
+++ nixos/profiles/graphical/gnome/default.nix
@@ -116,25 +116,4 @@ lib.mkIf config.services.desktopManager.gnome.enable {
       ".config/monitors.xml"
     ];
   };
-
-  security.pam.services = {
-    login.fprintAuth = false;
-    gdm-fingerprint.text = ''
-      auth       required                    pam_shells.so
-      auth       requisite                   pam_nologin.so
-      auth       requisite                   pam_faillock.so      preauth
-      auth       required                    ${pkgs.fprintd}/lib/security/pam_fprintd.so
-      auth       optional                    pam_permit.so
-      auth       required                    pam_env.so
-      auth       [success=ok default=1]      ${pkgs.gdm}/lib/security/pam_gdm.so
-      auth       optional                    ${pkgs.gnome-keyring}/lib/security/pam_gnome_keyring.so
-
-      account    include                     login
-
-      password   required                    pam_deny.so
-
-      session    include                     login
-      session    optional                    pam_gnome_keyring.so auto_start
-    '';
-  };
 }
