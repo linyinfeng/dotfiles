01251fd035a091906d3de4793cbeb36fabb65b7a
Lin Yinfeng <lin.yinfeng@outlook.com>
2025-06-27 21:22:24 +0800

    hosts/sparrow/hardware: add libva-v4l2

--- nixos/hosts/sparrow/_hardware.nix
+++ nixos/hosts/sparrow/_hardware.nix
@@ -1,65 +1,90 @@
-{ pkgs, ... }:
-{
-  hardware.enableAllFirmware = true;
-  hardware.deviceTree.filter = "mt8183-kukui-krane*.dtb";
+{ pkgs, lib, ... }:
+lib.mkMerge [
+  # kernel
+  {
+    hardware.enableAllFirmware = true;
+    hardware.deviceTree.filter = "mt8183-kukui-krane*.dtb";
 
-  # https://wiki.postmarketos.org/wiki/Lenovo_IdeaPad_Duet_Chromebook_(google-krane)
-  environment.etc."libinput/local-overrides.quirks".text = ''
-    [Touchpad pressure override]
-    MatchUdevType=touchpad
-    MatchName=Google Inc. Hammer
-    AttrPressureRange=20:10
+    # when using usb drive as root
+    # after switching root, loading this module causes root filesystem disconnection
+    boot.blacklistedKernelModules = [ "onboard_usb_dev" ];
+  }
 
-    [Google Chromebook Krane Stylus Digitizer]
-    MatchUdevType=tablet
-    MatchDeviceTree=*krane*
-    MatchBus=i2c
-    ModelChromebook=1
-    AttrPressureRange=1100:1000
-  '';
+  # systemd
+  {
+    systemd.package = pkgs.systemd.override {
+      withEfi = false;
+    };
+    boot.initrd.systemd.tpm2.enable = false;
+  }
 
-  systemd.package = pkgs.systemd.override {
-    withEfi = false;
-  };
+  # inputs
+  {
+    # https://wiki.postmarketos.org/wiki/Lenovo_IdeaPad_Duet_Chromebook_(google-krane)
+    environment.etc."libinput/local-overrides.quirks".text = ''
+      [Touchpad pressure override]
+      MatchUdevType=touchpad
+      MatchName=Google Inc. Hammer
+      AttrPressureRange=20:10
 
-  # when using usb drive as root
-  # after switching root, loading this module causes root filesystem disconnection
-  boot.blacklistedKernelModules = [ "onboard_usb_dev" ];
-  boot.initrd.systemd.tpm2.enable = false;
+      [Google Chromebook Krane Stylus Digitizer]
+      MatchUdevType=tablet
+      MatchDeviceTree=*krane*
+      MatchBus=i2c
+      ModelChromebook=1
+      AttrPressureRange=1100:1000
+    '';
+  }
 
-  home-manager.users.yinfeng.services.kanshi.settings =
-    let
-      embedded = "DSI-1";
-    in
-    [
-      {
-        output = {
-          criteria = embedded;
-          scale = 1.75;
-          transform = "90";
-        };
-      }
-      {
-        profile = {
-          name = "mobile";
-          outputs = [
-            { criteria = embedded; }
-          ];
-        };
-      }
+  # display
+  {
+
+    home-manager.users.yinfeng.services.kanshi.settings =
+      let
+        embedded = "DSI-1";
+      in
+      [
+        {
+          output = {
+            criteria = embedded;
+            scale = 1.75;
+            transform = "90";
+          };
+        }
+        {
+          profile = {
+            name = "mobile";
+            outputs = [
+              { criteria = embedded; }
+            ];
+          };
+        }
+      ];
+
+    # needed for window manager to manage display brightness
+    environment.systemPackages = with pkgs; [ wluma ];
+    environment.etc."xdg/wluma/config.toml".text = ''
+      [als.iio]
+      path = "/sys/bus/iio/devices"
+      thresholds = { 0 = "night", 20 = "dark", 80 = "dim", 250 = "normal", 500 = "bright", 800 = "outdoors" }
+
+      [[output.backlight]]
+      name = "DSI-1"
+      path = "/sys/class/backlight/backlight_lcd0"
+      capturer = "wayland"
+    '';
+    environment.global-persistence.user.directories = [ ".local/share/wluma" ];
+  }
+
+  # hardware video acceleration
+  {
+    hardware.graphics.extraPackages = [
+      pkgs.nur.repos.linyinfeng.libva-v4l2
     ];
-
-  # needed for window manager to manage display brightness
-  environment.systemPackages = with pkgs; [ wluma ];
-  environment.etc."xdg/wluma/config.toml".text = ''
-    [als.iio]
-    path = "/sys/bus/iio/devices"
-    thresholds = { 0 = "night", 20 = "dark", 80 = "dim", 250 = "normal", 500 = "bright", 800 = "outdoors" }
-
-    [[output.backlight]]
-    name = "DSI-1"
-    path = "/sys/class/backlight/backlight_lcd0"
-    capturer = "wayland"
-  '';
-  environment.global-persistence.user.directories = [ ".local/share/wluma" ];
-}
+    environment.systemPackages = with pkgs; [ libva-utils ];
+    environment.sessionVariables = {
+      LIBVA_DRIVER_NAME = "v4l2";
+      GST_VAAPI_ALL_DRIVERS = "1";
+    };
+  }
+]
