667da363f0204240046bf008ded0a850d55c6f4e
Lin Yinfeng <lin.yinfeng@outlook.com>
2025-08-05 17:48:58 +0800

    services/localtunnel: fix ports

--- nixos/profiles/services/localtunnel/default.nix
+++ nixos/profiles/services/localtunnel/default.nix
@@ -4,6 +4,7 @@
     script = ''
       "${pkgs.nur.repos.linyinfeng.rlt}/bin/localtunnel" server \
         --domain lt.li7g.com \
+        --secure \
         --port "${toString config.ports.localtunnel-endpoint}" \
         --proxy-port "${toString config.ports.localtunnel-request}"
     '';
@@ -15,11 +16,11 @@
   services.nginx.virtualHosts."lt.*" = {
     forceSSL = true;
     inherit (config.security.acme.tfCerts."li7g_com".nginxSettings) sslCertificate sslCertificateKey;
-    locations."/".proxyPass = "http://127.0.0.1:${toString config.ports.localtunnel-request}";
+    locations."/".proxyPass = "http://127.0.0.1:${toString config.ports.localtunnel-endpoint}";
   };
   services.nginx.virtualHosts."*.lt.li7g.com" = {
     forceSSL = true;
     inherit (config.security.acme.tfCerts."li7g_com".nginxSettings) sslCertificate sslCertificateKey;
-    locations."/".proxyPass = "http://127.0.0.1:${toString config.ports.localtunnel-endpoint}";
+    locations."/".proxyPass = "http://127.0.0.1:${toString config.ports.localtunnel-request}";
   };
 }
