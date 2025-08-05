b514769a84c497aa293599e1c8b4695b184a994f
Lin Yinfeng <lin.yinfeng@outlook.com>
2025-08-05 18:14:35 +0800

    Revert "services/localtunnel: fix ports"
    This reverts commit 667da363f0204240046bf008ded0a850d55c6f4e.

--- nixos/profiles/services/localtunnel/default.nix
+++ nixos/profiles/services/localtunnel/default.nix
@@ -4,7 +4,6 @@
     script = ''
       "${pkgs.nur.repos.linyinfeng.rlt}/bin/localtunnel" server \
         --domain lt.li7g.com \
-        --secure \
         --port "${toString config.ports.localtunnel-endpoint}" \
         --proxy-port "${toString config.ports.localtunnel-request}"
     '';
@@ -16,11 +15,11 @@
   services.nginx.virtualHosts."lt.*" = {
     forceSSL = true;
     inherit (config.security.acme.tfCerts."li7g_com".nginxSettings) sslCertificate sslCertificateKey;
-    locations."/".proxyPass = "http://127.0.0.1:${toString config.ports.localtunnel-endpoint}";
+    locations."/".proxyPass = "http://127.0.0.1:${toString config.ports.localtunnel-request}";
   };
   services.nginx.virtualHosts."*.lt.li7g.com" = {
     forceSSL = true;
     inherit (config.security.acme.tfCerts."li7g_com".nginxSettings) sslCertificate sslCertificateKey;
-    locations."/".proxyPass = "http://127.0.0.1:${toString config.ports.localtunnel-request}";
+    locations."/".proxyPass = "http://127.0.0.1:${toString config.ports.localtunnel-endpoint}";
   };
 }
