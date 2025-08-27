0739e52b3e26b58b8d1c575dcb016197b3a1e824
Lin Yinfeng <lin.yinfeng@outlook.com>
2025-08-27 08:15:27 +0800

    boot/binfmt: disable broken static emulators

--- nixos/profiles/boot/binfmt/default.nix
+++ nixos/profiles/boot/binfmt/default.nix
@@ -6,7 +6,7 @@
 }:
 {
   boot.binfmt = {
-    preferStaticEmulators = true;
+    # preferStaticEmulators = true; # TODO broken
     emulatedSystems = lib.remove pkgs.stdenv.hostPlatform.system config.lib.self.systems;
   };
 }
