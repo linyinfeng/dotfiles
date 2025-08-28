e4854d3fab3501f7baad2fb9679cf5420e95d8be
Lin Yinfeng <lin.yinfeng@outlook.com>
2025-08-28 18:23:21 +0800

    boot/binfmt: remove wrong workaround

--- nixos/profiles/boot/binfmt/default.nix
+++ nixos/profiles/boot/binfmt/default.nix
@@ -6,7 +6,7 @@
 }:
 {
   boot.binfmt = {
-    # preferStaticEmulators = true; # TODO broken
+    preferStaticEmulators = true;
     emulatedSystems = lib.remove pkgs.stdenv.hostPlatform.system config.lib.self.systems;
   };
 }
