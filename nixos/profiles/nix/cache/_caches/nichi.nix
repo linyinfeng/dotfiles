c822031f2ff2e96dcb97597536394c39bbdc1f68
Lin Yinfeng <lin.yinfeng@outlook.com>
2025-09-04 00:17:29 +0800

    nix/cache: remove cache.nichi.co

--- nixos/profiles/nix/cache/_caches/nichi.nix
+++
@@ -1,6 +0,0 @@
-{
-  nix.settings = {
-    substituters = [ "https://cache.nichi.co?priority=50" ];
-    trusted-public-keys = [ "hydra.nichi.co-0:P3nkYHhmcLR3eNJgOAnHDjmQLkfqheGyhZ6GLrUVHwk=" ];
-  };
-}
