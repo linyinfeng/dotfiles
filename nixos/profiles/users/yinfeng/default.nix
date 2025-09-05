5a4d634f1e7deb262c4082f3c1f9bff1aa9ae0fa
Lin Yinfeng <lin.yinfeng@outlook.com>
2025-09-05 17:14:18 +0800

    users/yinfeng: add gemini-cli

--- nixos/profiles/users/yinfeng/default.nix
+++ nixos/profiles/users/yinfeng/default.nix
@@ -18,81 +18,105 @@ in
     ./_atuin
   ];
 
-  users.users.${name} = {
-    inherit uid;
-    hashedPasswordFile = config.sops.secrets."user-password/${name}".path;
-    isNormalUser = true;
-    subUidRanges = [
-      {
-        startUid = 100000;
-        count = 65536;
-      }
-    ];
-    subGidRanges = [
-      {
-        startGid = 100000;
-        count = 65536;
-      }
-    ];
-    shell = pkgs.fish;
-    home = homeDirectory;
-    extraGroups =
-      with config.users.groups;
-      [
-        users.name
-        wheel.name
-        keys.name
-      ]
-      ++ groupNameIfPresent "audio"
-      ++ groupNameIfPresent "video"
-      ++ groupNameIfPresent "adbusers"
-      ++ groupNameIfPresent "libvirtd"
-      ++ groupNameIfPresent "transmission"
-      ++ groupNameIfPresent "networkmanager"
-      ++ groupNameIfPresent "tss"
-      ++ groupNameIfPresent "nix-access-tokens"
-      ++ groupNameIfPresent "nixbuild"
-      ++ groupNameIfPresent "hydra-builder-client"
-      ++ groupNameIfPresent "tg-send"
-      ++ groupNameIfPresent "service-mail"
-      ++ groupNameIfPresent "plugdev"
-      ++ groupNameIfPresent "acme"
-      ++ groupNameIfPresent "acmetf"
-      ++ groupNameIfPresent "windows"
-      ++ groupNameIfPresent "wireshark"
-      ++ groupNameIfPresent "feedbackd";
-
-    openssh.authorizedKeys.keyFiles = config.users.users.root.openssh.authorizedKeys.keyFiles;
-  };
-
-  sops.secrets."user-password/${name}" = {
-    neededForUsers = true;
-    sopsFile = config.sops-file.get "common.yaml";
-  };
-
-  environment.global-persistence.user.users = [ name ];
-  home-manager.users.${name} =
-    { ... }:
+  config = lib.mkMerge [
+    # basic
     {
-      home.global-persistence = {
+      users.users.${name} = {
+        inherit uid;
+        hashedPasswordFile = config.sops.secrets."user-password/${name}".path;
+        isNormalUser = true;
+        subUidRanges = [
+          {
+            startUid = 100000;
+            count = 65536;
+          }
+        ];
+        subGidRanges = [
+          {
+            startGid = 100000;
+            count = 65536;
+          }
+        ];
+        shell = pkgs.fish;
+        home = homeDirectory;
+        extraGroups =
+          with config.users.groups;
+          [
+            users.name
+            wheel.name
+            keys.name
+          ]
+          ++ groupNameIfPresent "audio"
+          ++ groupNameIfPresent "video"
+          ++ groupNameIfPresent "adbusers"
+          ++ groupNameIfPresent "libvirtd"
+          ++ groupNameIfPresent "transmission"
+          ++ groupNameIfPresent "networkmanager"
+          ++ groupNameIfPresent "tss"
+          ++ groupNameIfPresent "nix-access-tokens"
+          ++ groupNameIfPresent "nixbuild"
+          ++ groupNameIfPresent "hydra-builder-client"
+          ++ groupNameIfPresent "tg-send"
+          ++ groupNameIfPresent "service-mail"
+          ++ groupNameIfPresent "plugdev"
+          ++ groupNameIfPresent "acme"
+          ++ groupNameIfPresent "acmetf"
+          ++ groupNameIfPresent "windows"
+          ++ groupNameIfPresent "wireshark"
+          ++ groupNameIfPresent "feedbackd";
+
+        openssh.authorizedKeys.keyFiles = config.users.users.root.openssh.authorizedKeys.keyFiles;
+      };
+
+      sops.secrets."user-password/${name}" = {
+        neededForUsers = true;
+        sopsFile = config.sops-file.get "common.yaml";
+      };
+
+      environment.global-persistence.user.users = [ name ];
+      home-manager.users.${name}.home.global-persistence = {
         enable = true;
         home = homeDirectory;
       };
-
-      programs.git = {
-        userName = "Lin Yinfeng";
-        userEmail = "lin.yinfeng@outlook.com";
-        # do not sign by default
-        # signing.signByDefault = true;
+    }
+    # git and gpg
+    {
+      home-manager.users.${name} = {
+        programs.git = {
+          userName = "Lin Yinfeng";
+          userEmail = "lin.yinfeng@outlook.com";
+          # do not sign by default
+          # signing.signByDefault = true;
+        };
+        programs.gpg.publicKeys = [
+          {
+            source = ./_pgp/pub.asc;
+            trust = "ultimate";
+          }
+        ];
       };
-      programs.gpg.publicKeys = [
-        {
-          source = ./_pgp/pub.asc;
-          trust = "ultimate";
-        }
-      ];
-    };
+    }
 
-  environment.etc."nixos".source = "${homeDirectory}/Source/dotfiles";
-  programs.nh.flake = "${homeDirectory}/Source/dotfiles";
+    # gemini-cli
+    {
+      home-manager.users.yinfeng = {
+        programs.gemini-cli.enable = true;
+        programs.fish.interactiveShellInit = ''
+          export GEMINI_API_KEY="$(cat "${config.sops.secrets."gemini/api-key".path}")"
+        '';
+        home.global-persistence.directories = [ ".gemini" ];
+      };
+      sops.secrets."gemini/api-key" = {
+        sopsFile = config.sops-file.get "common.yaml";
+        owner = "yinfeng";
+      };
+    }
+
+    # system administration
+    {
+      environment.etc."nixos".source = "${homeDirectory}/Source/dotfiles";
+      programs.nh.flake = "${homeDirectory}/Source/dotfiles";
+    }
+  ];
+
 }
