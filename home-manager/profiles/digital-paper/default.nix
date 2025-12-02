5450eea85938ba0c34bf44472e389b56490a337e
Lin Yinfeng <lin.yinfeng@outlook.com>
2025-12-02 18:50:34 +0800

    hm/digital-paper: use own flake

--- home-manager/profiles/digital-paper/default.nix
+++ home-manager/profiles/digital-paper/default.nix
@@ -1,8 +1,8 @@
 { pkgs, ... }:
 {
   home.packages = with pkgs; [
-    nur.repos.linyinfeng.dpt-rp1-py
-    nur.repos.linyinfeng.activate-dpt
+    linyinfeng.dpt-rp1-py
+    linyinfeng.activate-dpt
   ];
 
   home.global-persistence.directories = [ ".dpapp" ];
