3ddf45917ddc760e4e05be6cc988ca80f766e3e5
Lin Yinfeng <lin.yinfeng@outlook.com>
2025-12-07 18:35:41 +0800

    services/fprintd: remove fprintd-blocker

--- nixos/profiles/services/fprintd/default.nix
+++ nixos/profiles/services/fprintd/default.nix
@@ -1,31 +1,4 @@
 { ... }:
 {
   services.fprintd.enable = true;
-  systemd.services.fprintd = {
-    unitConfig = {
-      ConditionPathExists = [ "!/run/fprintd-blocker" ];
-    };
-  };
-  systemd.services.fprintd-blocker = {
-    script = ''
-      touch /run/fprintd-blocker
-      systemctl stop fprintd.service
-    '';
-    preStop = ''
-      rm /run/fprintd-blocker
-    '';
-    serviceConfig = {
-      Type = "oneshot";
-      RemainAfterExit = true;
-    };
-  };
-  security.polkit.extraConfig = ''
-    polkit.addRule(function(action, subject) {
-      if (action.id == "org.freedesktop.systemd1.manage-units" &&
-          action.lookup("unit") === "fprintd-blocker.service" &&
-          subject.isInGroup("wheel")) {
-        return polkit.Result.YES;
-      }
-    });
-  '';
 }
