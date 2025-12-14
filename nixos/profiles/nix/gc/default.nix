2e17603bdc944a62adbbdd98f8260da65eaf4253
Lin Yinfeng <lin.yinfeng@outlook.com>
2025-12-14 12:04:47 +0800

    services/angrr: use developing 0.2.0

--- nixos/profiles/nix/gc/default.nix
+++ nixos/profiles/nix/gc/default.nix
@@ -1,10 +1,5 @@
-{ config, lib, ... }:
+{ ... }:
 {
-  nix.gc = {
-    automatic = true;
-    options = lib.mkDefault (
-      if config.system.types == [ "server" ] then "--delete-old" else "--delete-older-than 2d"
-    );
-  };
+  nix.gc.automatic = true;
   nix.settings.min-free = 1024 * 1024 * 1024; # bytes
 }
--- nixos/profiles/services/angrr/default.nix
+++ nixos/profiles/services/angrr/default.nix
@@ -2,7 +2,37 @@
 {
   services.angrr = {
     enable = true;
-    period = "7d";
     logLevel = "debug";
+    config = {
+      temporary-root-policies = {
+        direnv = {
+          path-regex = "/\\.direnv/";
+          period = "14d";
+        };
+        result = {
+          path-regex = "/result[^/]*$";
+          period = "3d";
+        };
+      };
+      profile-policies = {
+        system = {
+          profile-paths = [ "/nix/var/nix/profiles/system" ];
+          keep-since = "14d"; # do not keep based on time
+          keep-latest-n = 5; # keep latest
+          keep-current-system = true;
+          keep-booted-system = true;
+        };
+        user = {
+          profile-paths = [
+            "~/.local/state/nix/profiles/profile"
+            "/nix/var/nix/profiles/per-user/root/profile"
+          ];
+          keep-since = "1d";
+          keep-latest-n = 1;
+          keep-booted-system = false;
+          keep-current-system = false;
+        };
+      };
+    };
   };
 }
