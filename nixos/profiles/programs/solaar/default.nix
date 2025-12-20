79c61436bf0a63cf3f1b1e6fedcd3d2e3723933c
Lin Yinfeng <lin.yinfeng@outlook.com>
2025-12-20 14:54:25 +0800

    programs/solaar: add user service

--- nixos/profiles/programs/solaar/default.nix
+++ nixos/profiles/programs/solaar/default.nix
@@ -1,5 +1,16 @@
-{ pkgs, ... }:
+{ pkgs, lib, ... }:
 {
   services.udev.packages = with pkgs; [ logitech-udev-rules ];
   environment.systemPackages = with pkgs; [ solaar ];
+  environment.global-persistence.user.directories = [ ".config/solaar" ];
+  systemd.user.services.solaar = {
+    description = "Solaar Logitech Device Manager";
+    after = [ "graphical-session.target" ];
+    wantedBy = [ "graphical-session.target" ];
+    serviceConfig = {
+      ExecStart = "${lib.getExe pkgs.solaar} --window hide";
+      Restart = "on-failure";
+      RestartSec = "5s";
+    };
+  };
 }
