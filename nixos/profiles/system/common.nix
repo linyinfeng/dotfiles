d664ca8c26192690dd6f09ba9ec0a06e197135b2
Lin Yinfeng <lin.yinfeng@outlook.com>
2025-12-23 16:33:24 +0800

    system/common: enable all extra locales for testing

--- nixos/profiles/system/common.nix
+++ nixos/profiles/system/common.nix
@@ -2,6 +2,7 @@
 
 {
   i18n.defaultLocale = lib.mkDefault "en_US.UTF-8";
+  i18n.extraLocales = "all"; # for locale testing
   console.keyMap = lib.mkDefault "us";
   time.timeZone = lib.mkDefault "Asia/Shanghai";
   users.mutableUsers = lib.mkDefault false;
