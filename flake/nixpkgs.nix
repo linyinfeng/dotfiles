9756125ba4e60f73c0a6ea50bb89f7b315216b2d
Lin Yinfeng <lin.yinfeng@outlook.com>
2026-01-04 01:49:54 +0800

    nixpkgs: remove workarounds

--- flake/nixpkgs.nix
+++ flake/nixpkgs.nix
@@ -128,13 +128,6 @@ let
         {
           # maintained packages
           inherit (channels.latest) godns;
-
-          # TODO wait for https://nixpkgs-tracker.ocfox.me/?pr=475624
-          inherit (channels.latest) efitools;
-          # TODO wait for https://nixpkgs-tracker.ocfox.me/?pr=475445
-          inherit (channels.latest) lrzsz;
-          # TODO wait for https://nixpkgs-tracker.ocfox.me/?pr=475393
-          inherit (channels.latest) gemini-cli;
         }
         // (lib.optionalAttrs config.testingFlags.angrrNixpkgs {
           inherit (channels.angrr) angrr;
