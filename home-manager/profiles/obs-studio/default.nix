74875178069fb6d5b7760e5278c4225a541aee01
Lin Yinfeng <lin.yinfeng@outlook.com>
2026-01-06 20:23:33 +0800

    hm/obs-studio: remove obs-studio-wayland

--- home-manager/profiles/obs-studio/default.nix
+++ home-manager/profiles/obs-studio/default.nix
@@ -7,23 +7,6 @@
     ];
   };
 
-  xdg.desktopEntries.obs-studio-wayland = {
-    name = "OBS Studio (Wayland)";
-    genericName = "Streaming/Recording Software";
-    exec = "env QT_QPA_PLATFORM=wayland obs";
-    icon = "com.obsproject.Studio";
-    terminal = false;
-    type = "Application";
-    categories = [
-      "AudioVideo"
-      "Recorder"
-    ];
-    settings = {
-      "StartupWMClass" = "obs";
-    };
-    startupNotify = true;
-  };
-
   home.global-persistence = {
     directories = [ ".config/obs-studio" ];
   };
