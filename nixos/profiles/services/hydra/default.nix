3a5eeac794f176a197869d8964506f1652218ef8
Lin Yinfeng <lin.yinfeng@outlook.com>
2026-01-24 14:54:41 +0800

    services/hydra: refactor channel update

--- nixos/profiles/services/hydra/default.nix
+++ nixos/profiles/services/hydra/default.nix
@@ -10,79 +10,118 @@ let
     runtimeInputs = with pkgs; [
       jq
       systemd
-      postgresql
-      ripgrep
+      getFlakeCommit
+      channelUpdate
     ];
     text = ''
+      echo "--- begin event ---"
+      cat "$HYDRA_JSON" | jq
+      echo "--- end event ---"
+
       time=$(date --iso-8601=seconds)
       mkdir -p "/tmp/hydra-events"
       dump_file=$(mktemp "/tmp/hydra-events/$time-XXXXXX.json")
       cp "$HYDRA_JSON" "$dump_file"
-
       echo "event saved in: $dump_file"
-      echo "--- begin event ---"
-      cat "$dump_file"
-      echo "--- end   event ---"
 
-      hit=$(jq '
-        .project == "dotfiles" and
-        (.jobset == "main" or .jobset == "staging") and
-        .buildStatus == 0 and
-        .event == "buildFinished"
-      ' "$HYDRA_JSON")
-      echo "hit = $hit"
-
-      if [ "$hit" = "true" ]; then
-        job=$(jq --raw-output ".job" "$HYDRA_JSON")
-        echo "job = $job"
-
-        if [[ $job =~ ^nixos-([^/]*)\.(.*)$ && "$(jq --raw-output '.jobset' "$HYDRA_JSON")" == "main" ]]; then
-          host="''${BASH_REMATCH[1]}"
-          _system="''${BASH_REMATCH[2]}"
-
-          build_id=$(jq '.build' "$HYDRA_JSON")
-          flake_url=$(psql -t -U hydra -d hydra -c "
-                SELECT flake FROM jobsetevals
-                WHERE id = (SELECT eval FROM jobsetevalmembers
-                            WHERE build = $build_id
-                            LIMIT 1)
-                ORDER BY id DESC
-                LIMIT 1
-            ")
-          commit=$(echo "$flake_url" | rg --only-matching '/(\w{40})(\?.*)?$' --replace '$1')
-
-          mkdir -p "/tmp/dotfiles-channel-update"
-          update_file="/tmp/dotfiles-channel-update/$(basename "$dump_file")"
-          jq \
-            --arg host "$host" \
-            --arg commit "$commit" \
-            '{
-            host: $host,
-            commit: $commit,
-            outs: .products | map(.path),
-          }' "$HYDRA_JSON" >"$update_file"
-
-          echo "channel update: $update_file"
-          cat "$update_file"
-          systemctl start "dotfiles-channel-update@$(systemd-escape "$update_file")" --no-block
-
-        else
-          echo "job is not a nixos toplevel, or jobset is not main"
-
-          jq --raw-output '.products[].path' "$HYDRA_JSON" | (
-            while read -r out; do
-              echo "copy to cache: $out"
-              systemctl start "copy-cache-li7g-com@$(systemd-escape "$out").service" --no-block
-            done
-          )
-        fi
+      if [ "$(jq '.event == "buildFinished" and .buildStatus == 0' "$HYDRA_JSON")"  != "true" ]; then
+        echo "not a successful buildFinished event, exit."
+        exit 0
       fi
+
+      echo "copying outputs to cache..."
+      jq --raw-output '.outputs[].path' "$HYDRA_JSON" | while read -r out; do
+        echo "copying to cache: $out..."
+        systemctl start "copy-cache-li7g-com@$(systemd-escape "$out").service"
+        echo "done."
+      done
+
+      if [ "$(jq --from-file "${dotfilesChannelJobFilter}" "$HYDRA_JSON")" = "true" ]; then
+        echo "dotfiles channel job detected, update channel..."
+        host="$(jq --raw-output '.job | capture("^nixos-(?<host>[^/]*)\\.(.*)$").host' "$HYDRA_JSON")"
+        branch="nixos-tested-$host"
+        commit="$(get-flake-commit)"
+        channel-update "linyinfeng" "dotfiles" "$branch" "$commit"
+      fi
+    '';
+  };
+  dotfilesChannelJobFilter = pkgs.writeTextFile {
+    name = "nixos-job-filter.jq";
+    text = ''
+      .project == "dotfiles" and
+      .jobset == "main" and
+      (.job | test("^nixos-([^/]*)\\.(.*)$"))
+    '';
+  };
+  # currently github only
+  channelUpdate = pkgs.writeShellApplication {
+    name = "channel-update";
+    runtimeInputs = with pkgs; [
+      jq
+      git
+      util-linux
+    ];
+    text = ''
+      owner="$1"
+      repo="$2"
+      branch="$3"
+      commit="$4"
+      token=$(cat "$CREDENTIALS_DIRECTORY/github-token")
+
+      echo "updating $owner/$repo/$branch to $commit..."
+
+      cd /var/tmp
+      mkdir --parents "hydra-channel-update/$owner/$repo"
+      cd "hydra-channel-update/$owner/$repo"
+
+      (
+        echo "waiting for repository lock..."
+        flock 200
+        echo "enter critical section"
+
+        if [ ! -d "repo.git" ]; then
+          git clone "https://github.com/$owner/$repo.git" --filter=tree:0 --bare repo.git
+        fi
+
+        function repo-git {
+          git -C repo.git "$@"
+        }
+
+        repo-git remote set-url origin "https://-:$token@github.com/$owner/$repo.git"
+        repo-git fetch --all
+        if repo-git merge-base --is-ancestor "$commit" "$branch"; then
+          echo "commit $commit is already in branch $branch, skip."
+          exit 0
+        fi
+        repo-git push origin "$commit:$branch"
+
+        echo "leave critical section"
+      ) 200>lock
+    '';
+  };
+  getFlakeCommit = pkgs.writeShellApplication {
+    name = "get-flake-commit";
+    runtimeInputs = with pkgs; [
+      jq
+      postgresql
+      ripgrep
+    ];
+    text = ''
+      build_id=$(jq '.build' "$HYDRA_JSON")
+      flake_url=$(psql -t -U hydra -d hydra -c "
+            SELECT flake FROM jobsetevals
+            WHERE id = (SELECT eval FROM jobsetevalmembers
+                        WHERE build = $build_id
+                        LIMIT 1)
+            ORDER BY id DESC
+            LIMIT 1
+        ")
+      echo "$flake_url" | rg --only-matching '/(\w{40})(\?.*)?$' --replace '$1'
     '';
   };
 in
 {
   imports = [
-    ./_dotfiles-channel-update.nix
     ./_cache.nix
   ];
 
@@ -114,9 +153,6 @@ in
             jobs = .*
             excludeBuildFromContext = 1
           </githubstatus>
-          <runcommand>
-            command = "${lib.getExe hydraHook}"
-          </runcommand>
         '';
       };
       services.hydra.buildMachinesFiles = [ "/etc/nix-build-machines/hydra-builder/machines" ];
@@ -150,7 +186,10 @@ in
       ];
       sops.secrets."github_token_nano" = {
         predefined.enable = true;
-        restartUnits = [ "hydra.service" ];
+        restartUnits = [
+          "hydra-evaluator.service"
+          "hydra-queue-runner.service"
+        ];
       };
       sops.secrets."cache_li7g_com_key" = {
         predefined.enable = true;
@@ -196,6 +235,28 @@ in
     #     mode = "440";
     #   };
     # }
+
+    # run command
+    {
+      services.hydra.extraConfig = lib.mkAfter ''
+        <runcommand>
+          command = "${lib.getExe hydraHook}"
+        </runcommand>
+      '';
+      systemd.services.hydra-notify.serviceConfig.LoadCredential = [
+        "github-token:${config.sops.secrets."github_token_nano".path}"
+      ];
+      sops.secrets."github_token_nano".restartUnits = [ "hydra-notify.service" ];
+      security.polkit.extraConfig = ''
+        polkit.addRule(function(action, subject) {
+          if (action.id == "org.freedesktop.systemd1.manage-units" &&
+              RegExp('copy-cache-li7g-com@.+\.service').test(action.lookup("unit")) === true &&
+              subject.isInGroup("hydra")) {
+            return polkit.Result.YES;
+          }
+        });
+      '';
+    }
 
     {
       # email notifications
