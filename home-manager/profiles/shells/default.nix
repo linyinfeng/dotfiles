cdc033bf643f7d7900468bed6515717eceb0a601
Lin Yinfeng <lin.yinfeng@outlook.com>
2026-06-24 16:53:56 +0800

    hm/shells: update nushell configurations

--- home-manager/profiles/shells/default.nix
+++ home-manager/profiles/shells/default.nix
@@ -1,10 +1,29 @@
-{ lib, osConfig, ... }:
+{
+  lib,
+  osConfig,
+  pkgs,
+  ...
+}:
 {
   programs.fish.enable = true;
   programs.nushell.enable = true;
   programs.skim.enable = true;
   programs.zoxide.enable = true;
 
+  programs.nushell = {
+    configFile.text = ''
+      $env.config.show_banner = false
+    '';
+    plugins = with pkgs; [
+      nushell-plugin-polars
+      nushell-plugin-formats # additional formats
+      nushell-plugin-query # data selectors
+      nushell-plugin-semver
+      nushell-plugin-gstat # git status
+      nushell-plugin-skim
+      nushell-plugin-hcl
+    ];
+  };
   programs.fish.interactiveShellInit = ''
     # proxy
     ${lib.optionalString osConfig.networking.fw-proxy.enable "fenv source enable-proxy"}
