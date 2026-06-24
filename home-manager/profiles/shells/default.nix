3494699c68fbc6136ae3175db579782da1afe202
Lin Yinfeng <lin.yinfeng@outlook.com>
2026-06-24 22:17:32 +0800

    hm/shells: add direnv integration

--- home-manager/profiles/shells/default.nix
+++ home-manager/profiles/shells/default.nix
@@ -13,6 +13,21 @@
   programs.nushell = {
     configFile.text = ''
       $env.config.show_banner = false
+
+      # Direnv
+      use std/config env-conversions
+      # Initialize the PWD hook as an empty list if it doesn't exist
+      $env.config.hooks.env_change.PWD = $env.config.hooks.env_change.PWD? | default []
+      $env.config.hooks.env_change.PWD ++= [{||
+        if (which direnv | is-empty) {
+          # If direnv isn't installed, do nothing
+          return
+        }
+
+        direnv export json | from json | default {} | load-env
+        # If direnv changes the PATH, it will become a string and we need to re-convert it to a list
+        $env.PATH = do (env-conversions).path.from_string $env.PATH
+      }]
     '';
     plugins = with pkgs; [
       nushell-plugin-polars
