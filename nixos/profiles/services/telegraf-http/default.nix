62de1551f2e5f981f1c4ba298cac42e16517f3a2
Lin Yinfeng <lin.yinfeng@outlook.com>
2026-09-02 22:23:12 +0800

    services/telegraf-http: add git health check for forgejo

--- nixos/profiles/services/telegraf-http/default.nix
+++ nixos/profiles/services/telegraf-http/default.nix
@@ -52,6 +52,12 @@ let
         code = 200;
       }
     ];
+    git = [
+      {
+        url = "https://git.ts.li7g.com";
+        code = 200;
+      }
+    ];
     cache-overlay = [
       {
         url = "https://cache-overlay.ts.li7g.com/cache-li7g-com/zhl06z4lrfrkw5rp0hnjjfrgsclzvxpm.narinfo";
