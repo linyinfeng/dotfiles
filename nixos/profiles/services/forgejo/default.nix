1cd08c8db86362fb915e13bc9d3b27cf792cfa8e
Lin Yinfeng <lin.yinfeng@outlook.com>
2026-09-02 21:36:25 +0800

    services/forgejo: raise nginx client_max_body_size for git push

--- nixos/profiles/services/forgejo/default.nix
+++ nixos/profiles/services/forgejo/default.nix
@@ -5,6 +5,8 @@
     inherit (config.security.acme.tfCerts."li7g_com".nginxSettings) sslCertificate sslCertificateKey;
     locations."/" = {
       proxyPass = "http://127.0.0.1:${toString config.ports.forgejo}";
+      # git push packs can exceed nginx default 1m body limit
+      extraConfig = "client_max_body_size 0;";
     };
   };
 
