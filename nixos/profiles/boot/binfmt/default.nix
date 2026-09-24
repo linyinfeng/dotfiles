ef46d67494664b1072d030f8dc4564945a992d41
Lin Yinfeng <lin.yinfeng@outlook.com>
2026-09-24 15:56:02 +0800

    nixos/profiles/boot/binfmt: only emulate Linux systems
    emulatedSystems takes all of config.lib.self.systems, but the option only
    accepts Linux and Windows targets.

--- nixos/profiles/boot/binfmt/default.nix
+++ nixos/profiles/boot/binfmt/default.nix
@@ -7,6 +7,8 @@
 {
   boot.binfmt = {
     preferStaticEmulators = true;
-    emulatedSystems = lib.remove pkgs.stdenv.hostPlatform.system config.lib.self.systems;
+    emulatedSystems =
+      lib.remove pkgs.stdenv.hostPlatform.system
+        (lib.filter (system: (lib.systems.elaborate system).isLinux) config.lib.self.systems);
   };
 }
