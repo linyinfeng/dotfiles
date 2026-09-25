098b9297c7d25784bade93462e56ef9d064c7c32
Lin Yinfeng <lin.yinfeng@outlook.com>
2026-09-25 16:25:15 +0800

    flake: drop stale flat-flake input overrides

--- flake.nix
+++ flake.nix
@@ -36,13 +36,7 @@
     deploy-rs.inputs.utils.follows = "flake-utils";
 
     flat-flake.url = "github:linyinfeng/flat-flake";
-    flat-flake.inputs.crane.follows = "crane";
-    flat-flake.inputs.flake-compat.follows = "flake-compat";
-    flat-flake.inputs.flake-parts.follows = "flake-parts";
     flat-flake.inputs.nixpkgs.follows = "nixpkgs";
-    flat-flake.inputs.rust-overlay.follows = "rust-overlay";
-    flat-flake.inputs.systems.follows = "systems";
-    flat-flake.inputs.treefmt-nix.follows = "treefmt-nix";
 
     # nixos modules
 
