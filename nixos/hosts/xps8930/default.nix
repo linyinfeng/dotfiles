5afbd3c387492c52d202213748dd1b5897e66540
Lin Yinfeng <lin.yinfeng@outlook.com>
2026-09-26 14:02:38 +0800

    hosts/xps8930: disable networkd wait-online

--- nixos/hosts/xps8930/default.nix
+++ nixos/hosts/xps8930/default.nix
@@ -74,6 +74,7 @@ in
     enable = true;
     auto-login.enable = true;
   };
+  systemd.network.wait-online.enable = false; # networkd manages nothing here
   services.godns-multi = {
     ipv4.settings = {
       domains = [
